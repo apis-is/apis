7fd7271b103eec182e9a09c071f1374827c34236
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2013-03-06 01:14:51 +0000

    Endpoint logic separated from the server.js file

--- server.js
+++ server.js
@@ -17,33 +17,13 @@ exports.file = fileModule = require('file');
 exports.h = h = require('./lib/helpers.js');
 exports.endpoints = endpoints = require('./lib/endpoints.js');
 
-
 //Enables the use of posted params
 server.use(restify.bodyParser({ mapParams: true }));
 
 /**
  * Endpoints setup
- * Load all endpoints in the endpoints folder
- * walk is blocking on purpose because the server can't listen yet
  */
-console.log('Registering endpoints:');
-fileModule.walk('./endpoints', function(a, dirPath, dirs, files){
-	if(files){
-		files.forEach(function(file,key){
-			var fileName = file.replace('/index.js','')
-								.replace('.js','')
-								.replace('endpoints/','')
-								.replace('/','.'),
- 				requiredData = require('./'+file);
- 			endpoints.register(fileName,requiredData);
-		})
-	}
-	
-});
-
-//Register endpoins that do not have file associated.
-endpoints.register('phone',null);
-endpoints.register('help_out',null);
+endpoints.load();
 
 /**
  * Start the server
