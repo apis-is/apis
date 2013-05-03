864c9f7ea587f0b0a4f3355caed92b61883036b4
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-05-03 15:58:28 +0000

    Try statement made shorter + added triming to all values.

--- endpoints/cinema/index.js
+++ endpoints/cinema/index.js
@@ -13,44 +13,46 @@ var getMovies = function (req, res, next) {
 
 	request(url, function (error, response, body) {
 		if (error) return callback(err);
-		var obj = {
+
+		var $, obj = {
 			results: []
 		};
+
 		try {
-			var $ = cheerio.load( body );
-			var movies = $('.Kvikmyndir_TimeTable #divbox').find('.utanumMynd_new');
-
-			movies.each(function() {
-				var movie = $(this);
-				var st = [];
-				var showtimes = movie.find('[id^="myndbio"]');
-
-				showtimes.each(function() {
-					var showtime = {
-						theater: $(this).find('#bio a').text(),
-						schedule: []
-					};
-
-					$(this).find('.syningartimi_item').each(function() {
-						showtime.schedule.push( $(this).text() );
-					});
-
-					st.push( showtime );
-				});
-
-				obj.results.push({
-					title: movie.find('#mynd_titill a').html(),
-					released: movie.find('.mynd_titill_artal').text().match(/\d+/g)[0],
-					restricted: movie.find('.mynd_aldurstakmark img').attr('alt'),
-					imdb: movie.find('.imdbEinkunn').text(),
-					image: 'http://kvikmyndir.is' + movie.find('img.poster').attr('src'),
-					showtimes: st
-				});
-			});
-
+			$ = cheerio.load( body );
 		} catch (e) {
 			exports.logError( e );
 		}
+
+		var movies = $('.Kvikmyndir_TimeTable #divbox').find('.utanumMynd_new');
+
+		movies.each(function() {
+			var movie = $(this);
+			var st = [];
+			var showtimes = movie.find('[id^="myndbio"]');
+
+			showtimes.each(function() {
+				var showtime = {
+					theater: $(this).find('#bio a').text().trim(),
+					schedule: []
+				};
+
+				$(this).find('.syningartimi_item').each(function() {
+					showtime.schedule.push( $(this).text().trim() );
+				});
+
+				st.push( showtime );
+			});
+
+			obj.results.push({
+				title: movie.find('#mynd_titill a').html().trim(),
+				released: movie.find('.mynd_titill_artal').text().match(/\d+/g)[0].trim(),
+				restricted: movie.find('.mynd_aldurstakmark img').attr('alt').trim(),
+				imdb: movie.find('.imdbEinkunn').text().trim(),
+				image: 'http://kvikmyndir.is' + movie.find('img.poster').attr('src').trim(),
+				showtimes: st
+			});
+		});
 
 		res.json(200, obj);
 		return next();
