a7544fd94945654fc85c07411bd4fcaa55f191d3
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2013-05-04 11:45:04 +0000

    Revert "Endpoint /cinema/theaters added to the project." Revert This reverts commit d20a6e9f0cd662b9c093de878420b4e3317d0d8c.

--- endpoints/cinema/index.js
+++ endpoints/cinema/index.js
@@ -4,7 +4,6 @@ var h = require('../../lib/helpers.js');
 
 exports.setup = function ( server ) {
 	server.get({path: '/cinema', version: '1.0.0'}, getMovies);
-	server.get({path: '/cinema/theaters', version: '1.0.0'}, getTheaters);
 };
 
 /**
@@ -76,79 +75,6 @@ var getMovies = function (req, res, next) {
 				imdb: movie.find('.imdbEinkunn').text().trim(),
 				image: 'http://kvikmyndir.is' + imgURL,
 				showtimes: showtimes
-			});
-		});
-
-		res.json(200, obj);
-		return next();
-	});
-};
-
-/**
- * Fetches theaters that are showing movies today.
- * response - JSON: Theater data within an 'results' array.
- */
-var getTheaters = function (req, res, next) {
-	res.charSet = 'utf8';
-	var url = 'http://kvikmyndir.is/bio/syningatimar_bio/';
-
-	request(url, function (error, response, body) {
-		if (error) throw new Error( url + ' not responding correctly...' );
-
-		// Cheerio declared and then attemted to load.
-		var $;
-
-		try {
-			$ = cheerio.load( body );
-		} catch (e) {
-			throw new Error( 'Could not load the body with cherrio.' );
-		}
-
-		// Base object to be added to
-		// and eventually sent as a JSON response.
-		var obj = {
-			results: []
-		};
-
-		// DOM elements array containing all theaters.
-		var theaters = $('.Kvikmyndir_TimeTable').find('#utanumMynd_new');
-
-		// Loop through theaters
-		theaters.each(function() {
-			// This theater.
-			var theater = $(this);
-
-			// List of movies.
-			var movies = [];
-
-			// Loop through movies.
-			theater.find('#myndbio_new').each(function() {
-				// This movie.
-				var movie = $(this);
-
-				// Time schedule.
-				var schedule = [];
-
-				// Loop through each showtime on schedule today.
-				movie.find('#timi_new div').each(function () {
-					// Add time to the schedule.
-					schedule.push( $(this).find('.syningartimi_item').text().trim() );
-				});
-
-				// Append new movie to the list of movies.
-				movies.push({
-					title: movie.find('#bio a').text().trim(),
-					schedule: schedule
-				});
-			});
-
-			// Create an object of info
-			// and add it to the 'results' array.
-			obj.results.push({
-				name: theater.find('#mynd_titill a').text().trim(),
-				location: theater.find('.mynd_titill_artal').text().trim().replace(/(^\()|(\)$)/g, ''),
-				image: 'http://kvikmyndir.is' + theater.find('.mynd_plakat img').attr('src'),
-				movies: movies
 			});
 		});
 
--- endpoints/cinema/tests/integration_test.js
+++ endpoints/cinema/tests/integration_test.js
@@ -12,14 +12,3 @@ describe('cinema', function() {
         request(params, resultHandler);
     });
 });
-
-describe('cinema theaters', function() {
-    // The only thing that changes is the form attribute, so why not just re-use the object
-    var fieldsToCheckFor = ["image", "name", "location", "movies"];
-
-    it("should return an array of objects containing correct fields", function(done) {
-        var params = helpers.testRequestParams("/cinema/theaters");
-        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
-        request(params, resultHandler);
-    });
-});
