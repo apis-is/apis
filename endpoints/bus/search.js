1bc09334b03ceffdcd746c02def524b0e90438fd
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2013-05-11 17:35:39 +0000

    Made the branch flat and deleted unnecessary

--- endpoints/bus/search.js
+++
@@ -1,9 +0,0 @@
-exports.setup = function(server){
-	server.post({path: '/bus/search', version: '1.0.0'}, search); //Old
-	server.get({path: '/bus/search', version: '1.0.0'}, search);
-}
-
-var search = function(req, res, next) {
-	res.json(404,{error:"This api endpoint has been closed temporarily, because Bus.is changed it's markup."});
-	return next();
-}
