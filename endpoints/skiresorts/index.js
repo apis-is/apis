9cec31b2e20f264e8fb493a8ec3203a613ef38fc
Haraldur Karlsson <haraldur@haraldur.me>
2013-12-17 08:41:42 +0000

    Small scraping fixes along with name changes of variables

--- endpoints/skiresorts/index.js
+++ endpoints/skiresorts/index.js
@@ -38,20 +38,19 @@ app.get('/skiresorts', function(req, res){
 			return res.json(500,{error:'Could not parse body'});
 		}
 
-
 		var areas = {
 			'reykjavik': [
 				{
-					'name':'Höfuðborgarsvæðið',				
+					'area':'Höfuðborgarsvæðið',				
 					'resorts': [
 						{
 							'name': 'Bláfjöll',
 							'data_element': '#tab-1 .opening-info dt',
 							'webcam_url': 'http://cctv.skidasvaedi.is/current.jpg',
-							hill_name: function(element) {
+							slope_name: function(element) {
 								return $(element).text();
 							},
-							hill_open: function(element) {
+							slope_open: function(element) {
 								return $(element).next('dd').hasClass('open');
 							}
 						},
@@ -59,10 +58,10 @@ app.get('/skiresorts', function(req, res){
 							'name': 'Skálafell',
 							'data_element': '#tab-2 .opening-info dt',
 							'webcam_url': 'http://opnumskalafell.is/webcam/webcam/current.jpg',
-							hill_name: function(element) {
+							slope_name: function(element) {
 								return $(element).text();
 							},
-							hill_open: function(element) {
+							slope_open: function(element) {
 								return $(element).next('dd').hasClass('open');
 							}
 						}
@@ -71,16 +70,16 @@ app.get('/skiresorts', function(req, res){
 			],
 			'akureyri': [
 				{
-					'name':'Akureyri',
+					'area':'Akureyri',
 					'resorts': [
 						{
 							'name': 'Hlíðarfjall',
-							'data_element': '#fjallid li',
+							'data_element': '#fjallid ul:not(:first-child) li',
 							'webcam_url': 'http://www.hlidarfjall.is/static/files/vedurmyndir/hlidarfjall/current.jpg',
-							hill_name: function(element) {
+							slope_name: function(element) {
 								return $(element).text();
 							},
-							hill_open: function(element) {
+							slope_open: function(element) {
 								return $(element).find('img').attr('alt') == 'Opin';
 							}
 						}
@@ -89,20 +88,20 @@ app.get('/skiresorts', function(req, res){
 			],
 			'siglufjordur': [
 				{
-					'name':'Siglufjörður',
+					'area':'Siglufjörður',
 					'resorts': [
 						{
 							'name': 'Skarðsdalur',
 							'data_element': '#center .entryContent p:eq(1) img',
 							'webcam_url': 'http://157.157.79.85:8080/view/index.shtml',
-							hill_name: function(element) {
+							slope_name: function(element) {
 								var index = $(element).index(),
 									html = $(element).parent().html().replace(/&nbsp;/g, ''),
 									arr = html.split(/<img.*?\/>/g);
 
 								return arr[index+1];
 							},
-							hill_open: function(element) {
+							slope_open: function(element) {
 								return $(element).attr('src').indexOf('green') > -1;
 							}
 						}
@@ -111,43 +110,44 @@ app.get('/skiresorts', function(req, res){
 			],
 			'fjardarbyggd': [
 				{
-					'name':'Fjarðarbyggð',
+					'area':'Fjarðarbyggð',
 					'resorts': [
 						{
 							'name': 'Oddsskarð',
 							'data_element': '#promo tr',
 							'webcam_url': 'http://www.oddsskard.is/static/oddsskard/images/forsidumynd.jpg',
-							hill_name: function(element) {
+							slope_name: function(element) {
 								return $(element).find('td:eq(0)').text();
 							},
-							hill_open: function(element) {
+							slope_open: function(element) {
 								return $(element).find('td:eq(1)').text().indexOf('lok') == -1;
 							}
 						}
 					]
 				}
 			]
-		}
+		};
 
 		var obj = {results: []};
 
 		$.each(areas[setArea], function(k,v) {
-			var area = { 'name': v.name, 'resorts' : [] };
+			var area = { 'area': v.area, 'resorts' : [] };
 			$.each(v.resorts, function(kk, res) {
-				var resort = { 'name': res.name, 'webcam': res.webcam_url, 'hills': [] };
+				var resort = { 'name': res.name, 'webcam': res.webcam_url, 'slopes': [] };
 				data.find(res.data_element).each(function() {
-					var hill = { 'name': $.trim(res.hill_name($(this))), 'open': res.hill_open($(this)) };
-					resort.hills.push(hill);
+					var slope = { 'name': $.trim(res.slope_name($(this))), 'open': res.slope_open($(this)) };
+					resort.slopes.push(slope);
 				});
-				resort.hills.sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
 				// Fix because of dumb setup on Skálafell website.
-				if(resort.name == 'Skálafell') resort.hills.pop();
+				if(resort.name == 'Skálafell') resort.slopes.pop();
+				
+				resort.slopes.sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
 				
 				area.resorts.push(resort);
 			});
 			obj.results.push(area);
-		})
+		});
 
-		return res.cache(3600).json(obj);
+		return res.cache(3600).json(obj);		
 	});
 });
