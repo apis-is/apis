2ba4ea41789569a759d0f3e37568480c757d0379
Valur Hrafn Einarsson <valure09@ru.is>
2014-07-09 20:37:56 +0000

    intergration test for Skjar1 added

--- endpoints/tv/tests/integration_test.js
+++ endpoints/tv/tests/integration_test.js
@@ -1,6 +1,15 @@
 var request = require('request');
 var helpers = require('../../../lib/test_helpers.js');
 
+describe('tv - skjar1', function () {
+    it("should return an array of objects containing correct fields", function (done) {
+        var fieldsToCheckFor = ["series", "title", "originalTitle", "description", "live", "premier"];
+        var params = helpers.testRequestParams("/tv/skjar1");
+        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+        request.get(params, resultHandler);
+    });
+});
+
 describe('tv - stod2', function () {
     it("should return an array of objects containing correct fields", function (done) {
         var fieldsToCheckFor = ["series", "title", "originalTitle", "description", "live", "premier"];
