b4e363416f62b874ca1fc454bd69957907dc484d
mar <mar.nospam@anomy.net>
2014-07-26 00:49:55 +0000

    add rides endpoint - scraping Samferda.net for requests by A) drivers and B) passengers

---
+++ endpoints/rides/tests/integration_test.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var helpers = require('../../../lib/test_helpers.js');
+
+describe('rides root', function(){
+    it('should return info and endpoints', function (done) {
+        var fieldsToCheckFor = ['info', 'endpoints'];
+        var params = helpers.testRequestParams('/rides/');
+        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+        request.get(params, resultHandler);
+    });
+});
+
+[ 'samferda-drivers', 'samferda-passengers' ].forEach(function (api) {
+    var apiName = 'rides - ' + api;
+    describe(apiName, function () {
+        // this.timeout(8000);
+        var fieldsToCheckFor = ['link', 'from', 'to', 'date', 'time'];
+        it('should return an array of objects containing correct fields', function (done) {
+            var params = helpers.testRequestParams('/rides/' + api);
+            var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+            request.get(params, resultHandler);
+        });
+    });
+  });
