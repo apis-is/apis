823bffc9729fdd9263e1b190c6318807754b2737
Kristján Oddsson <koddsson@gmail.com>
2014-09-06 21:28:12 +0000

    Lower the particulates timeout.
    Since the particulates data cannot be retrieved from servers that are
    not in Iceland it's cool to decrease the timeout of them.

--- endpoints/particulates/tests/integration_test.js
+++ endpoints/particulates/tests/integration_test.js
@@ -5,8 +5,8 @@ var helpers = require('../../../lib/test_helpers.js');
 describe('particulates', function() {
     // The only thing that changes is the form attribute, so why not just re-use the object
     var fieldsToCheckFor = ["PM10nuna", "PM10medaltal", "Counter", "Dags", "nanariuppl"];
-    this.timeout(8000);
 
+    this.timeout(4000);
     it("should return an array of objects containing correct fields", function(done) {
         var params = helpers.testRequestParams("/particulates");
         var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
