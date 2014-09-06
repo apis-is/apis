c8880d3e36593fb9ff215ca692af7ec7a01b369c
Kristján Oddsson <koddsson@gmail.com>
2014-09-06 16:36:10 +0000

    Markup changes for declension endpoint. Fixes #96.

--- endpoints/declension/tests/integration_test.js
+++ endpoints/declension/tests/integration_test.js
@@ -3,11 +3,10 @@ var request = require('request'),
     helpers = require('../../../lib/test_helpers.js');
 
 describe('declension', function() {
-
-    //  it("should return an array of objects containing correct fields", function(done) {
-    //     var fieldsToCheckFor = ["predicate", "value", "category"];
-    //     var params = helpers.testRequestParams("/declension/laugavegur");
-    //     var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
-    //     request.get(params, resultHandler);
-    // });
+    it("should return an array of objects containing correct fields", function(done) {
+        var fieldsToCheckFor = ["predicate", "value", "category"];
+        var params = helpers.testRequestParams("/declension/laugavegur");
+        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+        request.get(params, resultHandler);
+    });
 });
