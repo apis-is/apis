05d8048f94f06a0c07cbfe872c270b096ec0d71b
Kristján Oddsson <koddsson@gmail.com>
2014-09-14 20:14:55 +0000

    Add /golf/teetimes endpoint.

--- endpoints/golf/index.js
+++ endpoints/golf/index.js
@@ -17,6 +17,36 @@ var getParamFromURL = function(url, param) {
     throw 'Key not found in query parameters';
 };
 
+app.get('/golf/teetimes', function(req, res) {
+  var clubId = req.query.club;
+  if(!clubId) {
+    return res.json(431, {error: 'Please provide a valid club id to lookup'});
+  }
+
+  request.get({
+    rejectUnauthorized: false, // http://stackoverflow.com/a/20091919
+    headers: {'User-Agent': h.browser()},
+    url: 'https://golf.is/pages/rastimar/rastimayfirlit/?club=' + clubId
+  }, function(err, response, html) {
+    if(err || response.statusCode !== 200) {
+      return res.json(500,{error: 'www.golf.is refuses to respond or give back data'});
+    }
+    $ = cheerio.load(html);
+    var rows = $('table.teeTimeTable tbody').children();
+    var time = '';
+    return res.cache().json({ results: _.map(rows, function(row) {
+	var row = $(row);
+	time = row.children('td.time').html() === null ? time : row.children('td.time').html();
+	return {
+	  time: time,
+	  name: $(row.children('td.name')).html(),
+	  club: $(row.children('td.club')).html(),
+	  handicap: $(row.children('td.handicap')).html()
+	};
+    })});
+  });
+});
+
 app.get('/golf/clubs', function(req, res) {
   request.get({
     rejectUnauthorized: false, // http://stackoverflow.com/a/20091919
