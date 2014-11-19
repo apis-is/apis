9bb8cfee490ebf5f87f545ecee13f7d8939cd118
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-11-19 20:33:07 +0000

    cleaned up the test runner

--- test/integration/endpoints.js
+++ test/integration/endpoints.js
@@ -1,11 +1,12 @@
-var fs = require('fs'),
-  	path = require('path'),
-	fileModule = require('file'),
-	testDir = '/tests',
-	testFileName = 'integration_test.js';
-
-	
+//Turn test mode on
 process.env.INTEGRATION = true;
+
+var fs = require('fs');
+var path = require('path');
+var fileModule = require('file');
+
+var testDir = '/tests';
+var testFileName = 'integration_test.js';
 
 describe('endpoint', function() {
 	it('should load the server and set everything up properly',function(done){
@@ -15,15 +16,21 @@ describe('endpoint', function() {
 
 		app.on('ready',function(){
 			fileModule.walkSync('./endpoints', function(dirPath, dirs, files){
-		        if (dirPath.indexOf(testDir) < 0) return;
-		        files.forEach(function(file){
-		            if (file != testFileName) return;
-		            var fullPath = dirPath + '/' + file;
-		            if (!fs.existsSync(fullPath)) return;
-                	    if (path.extname(fullPath) !== '.js') return;
-		            require('../../' + fullPath);
-		        });
+        if (dirPath.indexOf(testDir) < 0) return;
+
+        files.forEach(function(file){
+          if (file != testFileName) return;
+
+          var fullPath = dirPath + '/' + file;
+
+          if (!fs.existsSync(fullPath)) return;
+          if (path.extname(fullPath) !== '.js') return;
+          
+          require('../../' + fullPath);
+        });
+
 			});
+
 			done();
 		});
 	});
