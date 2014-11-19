ebc12e5c1f1982b51f2b7a44655da9f8f93148b0
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-11-19 20:42:15 +0000

    cleaned up the config

--- config/index.js
+++ config/index.js
@@ -1,14 +1,16 @@
-var _ = require('underscore'),
-    env = process.env.NODE_ENV ? process.env.NODE_ENV + '.config' : 'dev.config',
-    defaultConfig = require('./default.json'),
-    envConfig = {};
+var _ = require('underscore');
+var env = process.env.NODE_ENV ? process.env.NODE_ENV + '.config' : 'dev.config';
+var defaultConfig = require('./default.json');
+var envConfig = {};
 
-if (process.env.INTEGRATION) env = 'integration';
+if (process.env.INTEGRATION){
+  env = 'integration';
+}
 
 try {
-    envConfig = require('./' + env + '.json');
+  envConfig = require('./' + env + '.json');
 } catch (e) {
-    console.log(e.code == 'MODULE_NOT_FOUND' ? 'Please create a dev.config.json file in the config folder' : e);
+  console.log(e.code == 'MODULE_NOT_FOUND' ? 'Please create a dev.config.json file in the config folder' : e);
 }
 
 module.exports = _.extend(defaultConfig, envConfig);
