f3fd7ba7a78fc1b98e89f0439213e7e576588838
Hermann Björgvin Haraldsson <hermann3646@gmail.com>
2015-01-14 16:06:41 +0000

    Default reponse change for /tv
    Adds array of objects containing Icelandic TV channel names along with their endpoints. Backwards compatible and non breaking change that helps you build an application without updating the routes for channels

--- endpoints/tv/index.js
+++ endpoints/tv/index.js
@@ -18,7 +18,18 @@ app.get('/tv', function (req, res) {
             stod2bio: '/tv/stod2bio',
             stod3: '/tv/stod3',
             skjar1: '/tv/skjar1'
-          }
+          },
+          channels: [
+            {name: 'Rúv', endpoint: '/tv/ruv/'},
+            {name: 'Rúv íþróttir', endpoint: '/tv/ruvithrottir/'},
+            {name: 'Stöð 2', endpoint: '/tv/stod2/'},
+            {name: 'Stöð 2 Sport', endpoint: '/tv/stod2sport'},
+            {name: 'Stöð 2 Sport 2', endpoint: '/tv/stod2sport2'},
+            {name: 'Stöð 2 Gull', endpoint: '/tv/stod2gull'},
+            {name: 'Stöð 2 Bíó', endpoint: '/tv/stod2bio'},
+            {name: 'Stöð 2', endpoint: '/tv/stod3'},
+            {name: 'Skjár 1', endpoint: '/tv/skjar1}'
+          ]
         }
       ]
     }
