a7abd163c96341937962b4e55bd32c0f3fa3c4b9
Hermann Björgvin Haraldsson <hermann3646@gmail.com>
2015-02-01 00:53:32 +0000

    fucked up my initial commit
    .....

--- endpoints/torrents/tests/integration_test.js
+++ endpoints/torrents/tests/integration_test.js
@@ -14,10 +14,10 @@ describe('torrents', function() {
     // "clickcount":15600,
     // "hidden":false}
     
-    var fieldsToCheckFor = ["image", "imdb", "released", "restricted", "showtimes", "title"];
+    var fieldsToCheckFor = ["id", "created_at", "updated_at", "domain", "ssl", "up", "inviteonly", "clickcount", "hidden"];
 
     it("should return an array of objects containing correct fields", function(done) {
-        var params = helpers.testRequestParams("/cinema");
+        var params = helpers.testRequestParams("/torrents");
         var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
         request(params, resultHandler);
     });
