5a8034de5272770955698711f5e5b6dd8ca192c4
Hermann Björgvin Haraldsson <hermann3646@gmail.com>
2015-02-02 13:28:28 +0000

    removed redundant comment
    Tis been vanquished

--- endpoints/torrents/tests/integration_test.js
+++ endpoints/torrents/tests/integration_test.js
@@ -3,17 +3,6 @@ var assert = require('assert');
 var helpers = require('../../../lib/test_helpers.js');
 
 describe('torrents', function() {
-    // I just copied the cinema test so i hope this is correct
-    // {"id":53,
-    // "created_at":"2000-01-01T00:00:00.000Z",
-    // "updated_at":"2015-01-22T22:57:24.791Z",
-    // "domain":"icetracker.org",
-    // "ssl":false,
-    // "up":false,
-    // "inviteonly":false,
-    // "clickcount":15600,
-    // "hidden":false}
-    
     var fieldsToCheckFor = ["id", "created_at", "updated_at", "domain", "ssl", "up", "inviteonly", "clickcount", "hidden"];
 
     it("should return an array of objects containing correct fields", function(done) {
