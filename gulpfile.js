5f3e01687ac75ddb9c0b22fc8295c22289e75648
Kristján Oddsson <koddsson@gmail.com>
2015-02-08 21:38:27 +0000

    Gulp pipeline baby!

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var to5 = require('gulp-6to5');
+var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
+
+gulp.task('clean', function () {
+    return gulp.src('dist/*')
+      .pipe(vinylPaths(del));
+});
+
+gulp.task('test', ['build'], function() {
+    return gulp.src('dist/test.js', {read: false})
+      .pipe(mocha({reporter: 'spec', timeout: 10000}));
+});
+
+gulp.task('build', ['clean'], function() {
+    return gulp.src('src/**/*.js')
+      .pipe(to5())
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch('src/**/*.js', ['default']);  
+    nodemon({ script: 'dist/index.js' })
+      .on('change', ['build'])
+      .on('restart', function () {
+          console.log('restarted!');
+      });
+});
+
+gulp.task('default', ['build']);
