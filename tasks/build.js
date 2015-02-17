6769a554e5c37be247a8b9f2655709a2a76d2976
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-02-17 20:12:19 +0000

    Stop runnin clean on build, since it is not running in the correct order as is

--- tasks/build.js
+++ tasks/build.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(gulp) {
-  gulp.task('build', ['clean'], function() {
+  gulp.task('build', function() {
     return gulp.start('scripts', 'styles', 'templates', 'images', 'copy');
   });
 };
