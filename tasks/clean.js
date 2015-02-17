e6b91821f8a6e1f3281721152373828fdb45b7cf
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-02-17 20:12:31 +0000

    Return del function in clean task

--- tasks/clean.js
+++ tasks/clean.js
@@ -3,9 +3,8 @@
 module.exports = function(gulp) {
   var del = require('del');
   gulp.task('clean', function() {
-    del(gulp.cfg.env.dir + '/**/*', function (err, deletedFiles) {
+    return del(gulp.cfg.env.dir + '/**/*', function (err, deletedFiles) {
       gulp.plugin.util.log('Files deleted:', deletedFiles.join(', '));
     });
-    return gulp.src(gulp.cfg.env.dir +'/*', {read: false});
   });
 };
