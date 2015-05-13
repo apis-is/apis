94b7ae7a95af2b5d0ef6c1a1679510bb30fc5829
Kristján Oddsson <koddsson@gmail.com>
2015-05-13 17:19:21 +0000

    Fix tests for vinbud endpoint.

--- endpoints/vinbud/test.js
+++ endpoints/vinbud/test.js
@@ -7,6 +7,7 @@ describe('Looking up opening times', function() {
 });
 
 describe('Looking up items', function() {
+  this.timeout(20000);
   it('should succeed', function(done) {
     endpoint.tester('/items').expect(200, done);
   });
