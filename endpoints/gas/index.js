e0aa2439bfbd3278b69d841013f117109c878f85
Kristján Oddsson <koddsson@gmail.com>
2015-06-10 21:39:39 +0000

    Revert "Install dependencies when running with npm."
    This reverts commit 7b848bbc8842b11b02466d33bce981e3813005c7.

--- endpoints/gas/index.js
+++
@@ -1,7 +0,0 @@
-var endpoint = module.exports = require('apis-endpoint')();
-
-var car = require('bluebird').promisifyAll(require('car'));
-
-endpoint.get('/is/:carPlate', function(data) {
-  return car.isAsync(data.carPlate);
-});
