4d6b690bf4d2c40d285a8e4b6a7edaa7a7268c57
Haraldur Karlsson <haraldurkarls@gmail.com>
2015-07-09 21:36:12 +0000

    added carparks endpoint

---
+++ endpoints/carparks/tests/integration_test.js
@@ -0,0 +1,12 @@
+var request = require('request');
+var assert = require('assert');
+var helpers = require('../../../lib/test_helpers.js');
+
+describe('carparks', function() {
+    it("should return an array of objects containing correct fields", function(done) {
+        var fieldsToCheckFor = ["name","address","parkingSpaces","coordinates"];
+        var params = helpers.testRequestParams("/carparks");
+        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+        request.get(params, resultHandler);
+    });
+});
