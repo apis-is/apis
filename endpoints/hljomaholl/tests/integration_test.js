52714fd9b02d46c4312a9a4ae9d58bd73943fdf7
Kristján Oddsson <koddsson@gmail.com>
2015-07-10 18:38:16 +0000

    Fix code style

--- endpoints/hljomaholl/tests/integration_test.js
+++ endpoints/hljomaholl/tests/integration_test.js
@@ -3,14 +3,17 @@ var assert = require('assert');
 var helpers = require('../../../lib/test_helpers.js');
 
 describe('hljomaholl', function() {
-    // The only thing that changes is the form attribute, so why not just re-use the object
-    var fieldsToCheckFor = ['date', 'time', 'image','title', 'description','location', 'buyTicketURL', 'moreInfoURL'];
+  // The only thing that changes is the form attribute, so why not just re-use the object
+  var fieldsToCheckFor = [
+    'date', 'time', 'image', 'title', 'description', 'location', 'buyTicketURL',
+    'moreInfoURL'
+  ];
 
-    it("should return an array of objects containing correct fields", function(done) {
-        var params = helpers.testRequestParams("/hljomaholl");
-        var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
-        request(params, resultHandler);
-    });
+  it ('should return an array of items with correct fields', function(done) {
+    var params = helpers.testRequestParams('/hljomaholl');
+    var resultHandler = helpers.testRequestHandlerForFields(
+      done, fieldsToCheckFor, null
+    );
+    request(params, resultHandler);
+  });
 });
-
-
