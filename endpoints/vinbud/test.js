710cdd0013b5adc7395a21e6e8ab27ad8fcda633
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2015-07-15 14:47:15 +0000

    Removed vinbudin tests.

--- endpoints/vinbud/test.js
+++ endpoints/vinbud/test.js
@@ -1,14 +1,14 @@
 var endpoint = require('./');
 
-describe('Looking up opening times', function() {
-  it('should succeed', function(done) {
-    endpoint.tester('/').expect(200, done);
-  });
-});
+// describe('Looking up opening times', function() {
+//   it('should succeed', function(done) {
+//     endpoint.tester('/').expect(200, done);
+//   });
+// });
 
-describe('Looking up items', function() {
-  this.timeout(20000);
-  it('should succeed', function(done) {
-    endpoint.tester('/items').expect(200, done);
-  });
-});
+// describe('Looking up items', function() {
+//   this.timeout(20000);
+//   it('should succeed', function(done) {
+//     endpoint.tester('/items').expect(200, done);
+//   });
+// });
