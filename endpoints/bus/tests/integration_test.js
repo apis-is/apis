ff27b0cfc8383869026ccbf30b100312e741e93f
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2015-10-28 23:43:17 +0000

    Added bus tests again

--- endpoints/bus/tests/integration_test.js
+++ endpoints/bus/tests/integration_test.js
@@ -11,27 +11,27 @@ describe('bus', function() {
         helpers.assertPresenceOfFields(["unixTime","x","y","from","to"], busses);
     };
 
-    // describe('realtime', function() {
-    //     describe('searching a single bus', function() {
-    //         it("should return an array of objects containing correct fields", function(done) {
-    //             var params = helpers.testRequestParams("/bus/realtime?busses=1");
-    //             var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor, customCheck);
-    //             request.get(params, resultHandler);
-    //         });
-    //     });
-    //     describe('searching multiple busses', function() {
-    //         it("should return an array of objects containing correct fields", function(done) {
-    //             var params = helpers.testRequestParams("/bus/realtime?busses=1,5");
-    //             var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor, customCheck);
-    //             request.get(params, resultHandler);
-    //         });
-    //     });
-    //     describe('searching for a non existant bus', function() {
-    //         it("should return an array of objects containing correct fields", function(done) {
-    //             var params = helpers.testRequestParams("/bus/realtime?busses=999");
-    //             var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
-    //             request.get(params, resultHandler);
-    //         });
-    //     });
-    // });
+    describe('realtime', function() {
+        describe('searching a single bus', function() {
+            it("should return an array of objects containing correct fields", function(done) {
+                var params = helpers.testRequestParams("/bus/realtime?busses=1");
+                var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor, customCheck);
+                request.get(params, resultHandler);
+            });
+        });
+        describe('searching multiple busses', function() {
+            it("should return an array of objects containing correct fields", function(done) {
+                var params = helpers.testRequestParams("/bus/realtime?busses=1,5");
+                var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor, customCheck);
+                request.get(params, resultHandler);
+            });
+        });
+        describe('searching for a non existant bus', function() {
+            it("should return an array of objects containing correct fields", function(done) {
+                var params = helpers.testRequestParams("/bus/realtime?busses=999");
+                var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor);
+                request.get(params, resultHandler);
+            });
+        });
+    });
 });
