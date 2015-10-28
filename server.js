1eb23e92bcf72c2670dd6b4eaca24390fcf47cb8
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2015-10-28 23:40:30 +0000

    Some error handling

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var fileModule = require('file');
 var cache = require('./lib/cache');
 var cors = require('./lib/cors');
 var EE = require('events').EventEmitter;
+var statuses = require('statuses');
+
 
 /**
  * Set the spacing to 0 for shorter output
@@ -36,6 +38,43 @@ fileModule.walkSync('./endpoints', function iterateEndpoints(dirPath, dirs, endp
     function requireEndpoint(endpoint) {
         if (endpoint.indexOf('.DS_Store') === -1) require('./' + dirPath + '/' + endpoint);
     }
+});
+
+app.use(function(error, req, res, next){
+  var code = 500;
+  var message = 'Unknown error';
+
+  if (typeof error === 'number') {
+    //next(404)
+    code = error
+    message = statuses[code] || message;
+  }else if (typeof error === 'object' && error.message && error.message.length === 3 && !isNaN(error.message)) {
+    //throw Error(404)
+    code = parseInt(error.message,10);
+    message = statuses[code] || message;
+
+  }else if(typeof error === 'object' && !error.code){
+    //throw Error('foo') or throw Error('404|foo')
+    
+    var pos = error.message.indexOf('|');
+
+    if(!!~pos){
+      code = error.message.slice(0,pos);
+      message = error.message.slice(pos + 1,error.message.length);
+    }else{
+      message = error.message;
+    }
+  }else{
+    message = error.message;
+  }
+
+  code = parseInt(code);
+
+  if(res.headersSent){
+    return console.error('Headers already sent');
+  }
+  
+  res.status(code).json({error: message});
 });
 
 /**
