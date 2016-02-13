e4b5d276259830c93cfd1c1b35b6c064ee142dd9
Kristján Oddsson <koddsson@gmail.com>
2016-02-13 08:12:10 +0000

    Fix URL for golf endpoint

--- endpoints/golf/index.js
+++ endpoints/golf/index.js
@@ -26,10 +26,10 @@ app.get('/golf/teetimes', function(req, res) {
   request.get({
     rejectUnauthorized: false, // http://stackoverflow.com/a/20091919
     headers: {'User-Agent': h.browser()},
-    url: 'https://golf.is/pages/rastimar/rastimayfirlit/?club=' + clubId
+    url: 'http://mitt.golf.is/pages/rastimar/rastimayfirlit/?club=' + clubId
   }, function(err, response, html) {
     if(err || response.statusCode !== 200) {
-      return res.json(500,{error: 'www.golf.is refuses to respond or give back data'});
+      return res.json(500,{error: 'mitt.golf.is refuses to respond or give back data'});
     }
     $ = cheerio.load(html);
     var rows = $('table.teeTimeTable tbody').children();
@@ -51,10 +51,10 @@ app.get('/golf/clubs', function(req, res) {
   request.get({
     rejectUnauthorized: false, // http://stackoverflow.com/a/20091919
     headers: {'User-Agent': h.browser()},
-    url: 'https://www.golf.is/pages/rastimar/'
+    url: 'http://mitt.golf.is/pages/rastimar/'
   }, function(err, response, html) {
     if(err || response.statusCode !== 200)
-      return res.json(500,{error: 'www.golf.is refuses to respond or give back data'});
+      return res.json(500,{error: 'mitt.golf.is refuses to respond or give back data'});
     $ = cheerio.load(html);
     var rows = $('table.golfTable tr').slice(2); // Skip the first element.
     return res.cache(3600).json({ results: _.map(rows, function(row) {
