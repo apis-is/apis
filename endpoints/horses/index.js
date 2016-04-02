a89354c35bb15a435ba70b07a7c7fe5942fa61d3
Loknar <svefgud@gmail.com>
2016-04-02 21:18:20 +0000

    accidental invalid header

--- endpoints/horses/index.js
+++ endpoints/horses/index.js
@@ -14,7 +14,7 @@ function query_data(id, name, origin, microchip, callback) {
 	var headers = {
 		'User-Agent': h.browser(),
 		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		'Content-Type:': 'application/x-www-form-urlencoded'
+		'Content-Type': 'application/x-www-form-urlencoded'
 	}
 	var url = 'http://www.worldfengur.com/freezone_horse.jsp?c=EN';
 	var form_data = 'fnr=' + id +
