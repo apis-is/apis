fa86ded70c9a2567b2d438d296ced9d299510975
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 20:18:23 +0000

    Fix linting for particulates endpoint

--- endpoints/particulates/index.js
+++ endpoints/particulates/index.js
@@ -1,26 +1,26 @@
-var request = require('request')
-var parseString = require('xml2js').parseString
-var app = require('../../server')
+import request from 'request'
+import xml2js from 'xml2js'
+import app from '../../server'
+const parseString = xml2js.parseString
 
-app.get('/particulates', function (req, res) {
-
+app.get('/particulates', (req, res) => {
   request.get({
-    url: 'http://www.loft.rvk.is/xml/Xsvifryk.xml'
-  }, function (err, response, xml) {
+    url: 'http://www.loft.rvk.is/xml/Xsvifryk.xml',
+  }, (err, response, xml) => {
     if (err || response.statusCode !== 200) {
       return res.status(500).json({
-        error: 'www.loft.rvk.is refuses to respond or give back data'
+        error: 'www.loft.rvk.is refuses to respond or give back data',
       })
     }
 
-    var particulates = []
-    parseString(xml, { explicitRoot: false }, function (err, result) {
+    const particulates = []
+    parseString(xml, { explicitRoot: false }, (parseError, result) => {
       particulates.push({
         PM10nuna: result.PM10nuna[0],
         PM10medaltal: result.PM10medaltal[0],
         Counter: result.Counter[0],
         Dags: result.Dags[0],
-        nanariuppl: result.nanariuppl[0]
+        nanariuppl: result.nanariuppl[0],
       })
 
       return res.json({ results: particulates })
