03c1fbd29889abe6705a918d16b73d11ae46f690
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 19:27:38 +0000

    Fix linting for declension endpoint tests

--- endpoints/declension/tests/integration_test.js
+++ endpoints/declension/tests/integration_test.js
@@ -1,12 +1,11 @@
-var request = require('request'),
-  assert = require('assert'),
-  helpers = require('../../../lib/test_helpers.js')
+import request from 'request'
+import helpers from '../../../lib/test_helpers.js'
 
-describe('declension', function () {
-  it('should return an array of objects containing correct fields', function (done) {
-    var fieldsToCheckFor = ['predicate', 'value', 'category']
-    var params = helpers.testRequestParams('/declension/laugavegur')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+describe('declension', () => {
+  it('should return an array of objects containing correct fields', (done) => {
+    const fieldsToCheckFor = ['predicate', 'value', 'category']
+    const params = helpers.testRequestParams('/declension/laugavegur')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
   })
 })
