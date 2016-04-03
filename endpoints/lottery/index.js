720b7c1bff1bf023acdaa6325348e16f57c36a5e
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 20:09:38 +0000

    Fix linting for lottery endpoint

--- endpoints/lottery/index.js
+++ endpoints/lottery/index.js
@@ -1,15 +1,59 @@
-var request = require('request')
-var app = require('../../server')
-var cheerio = require('cheerio')
+import request from 'request'
+import app from '../../server'
+import cheerio from 'cheerio'
 
 
-var getLotto = function (req, res, next) {
-  getLottery(function (err, body) {
+const parseList = function (body) {
+  let $
+  try {
+    $ = cheerio.load(body)
+  } catch (error) {
+    throw new Error('Could not parse body')
+  }
+
+  const results = []
+
+  const tr = $('table').eq(1).find('tr')
+
+  tr.each(function (i) {
+    if (i === 0) return
+    const td = $(this).find('td')
+
+    results.push({
+      date: td.eq(0).text().trim(),
+      lotto: td.eq(1).html().match(/\d{1,2}/g).join(' ').replace(/(\d{1,2})$/, '($1)'),
+      joker: td.eq(2).text().trim(),
+      prize: td.eq(3).text().trim(),
+      link: `http://lotto.is${td.eq(4).find('a').attr('href').trim()}`,
+    })
+  })
+
+  return results
+}
+
+const getLottery = function (callback, providedUrl) {
+  const url = providedUrl || 'https://igvefur.lotto.is/lottoleikir/urslit/lotto/'
+
+  const params = { url }
+
+  request(params, (error, res, body) => {
+    if (error) return callback(error)
+
+    if (res.statusCode !== 200) {
+      return callback(new Error(`HTTP error from endpoint, status code ${res.statusCode}`))
+    }
+
+    return callback(null, body)
+  })
+}
+
+const getLotto = (req, res, next) => {
+  getLottery((err, body) => {
     if (err) {
       return next(502)
     }
     return res.cache(3600).json({
-      results: parseList(body)
+      results: parseList(body),
     })
   })
 }
@@ -17,68 +61,23 @@ var getLotto = function (req, res, next) {
 app.get('/lottery', getLotto)
 app.get('/lottery/lotto', getLotto)
 
-app.get('/lottery/vikingalotto', function (req, res, next) {
-  getLottery(function (err, body) {
+app.get('/lottery/vikingalotto', (req, res, next) => {
+  getLottery((err, body) => {
     if (err) {
       return next(502)
     }
     return res.cache(3600).json({
-      results: parseList(body)
+      results: parseList(body),
     })
   }, 'https://games.lotto.is/lottoleikir/urslit/vikingalotto/')
 })
-app.get('/lottery/eurojackpot', function (req, res, next) {
-  getLottery(function (err, body) {
+app.get('/lottery/eurojackpot', (req, res, next) => {
+  getLottery((err, body) => {
     if (err) {
       return next(502)
     }
     return res.cache(3600).json({
-      results: parseList(body)
+      results: parseList(body),
     })
   }, 'https://games.lotto.is/lottoleikir/urslit/eurojackpot/')
 })
-
-var getLottery = function (callback, url) {
-  url = url || 'https://igvefur.lotto.is/lottoleikir/urslit/lotto/'
-
-  var params = {
-    url: url
-  }
-
-  request(params, function (error, res, body) {
-    if (error) return callback(error)
-
-    if (res.statusCode != 200) {
-      return callback(new Error('HTTP error from endpoint, status code ' + res.statusCode))
-    }
-
-    return callback(null, body)
-  })
-}
-
-var parseList = function (body) {
-  try {
-    var $ = cheerio.load(body)
-  } catch (error) {
-    throw new Error('Could not parse body')
-  }
-
-  var results = []
-
-  var tr = $('table').eq(1).find('tr')
-
-  tr.each(function (i) {
-    if (i === 0) return
-    var td = $(this).find('td')
-
-    results.push({
-      date:  td.eq(0).text().trim(),
-      lotto: td.eq(1).html().match(/\d{1,2}/g).join(' ').replace(/(\d{1,2})$/, '($1)'),
-      joker: td.eq(2).text().trim(),
-      prize: td.eq(3).text().trim(),
-      link:  'http://lotto.is' + td.eq(4).find('a').attr('href').trim()
-    })
-  })
-
-  return results
-}
--- endpoints/lottery/tests/integration_test.js
+++ endpoints/lottery/tests/integration_test.js
@@ -1,44 +1,43 @@
-var request = require('request')
-var assert = require('assert')
-var helpers = require('../../../lib/test_helpers.js')
+import request from 'request'
+import helpers from '../../../lib/test_helpers.js'
 
-describe.skip('lotto', function () {
+describe.skip('lotto', () => {
     // The only thing that changes is the form attribute, so why not just re-use the object
-  var fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
+  const fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
 
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/lottery')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/lottery')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
-describe.skip('lotto', function () {
+describe.skip('lotto', () => {
     // The only thing that changes is the form attribute, so why not just re-use the object
-  var fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
+  const fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
 
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/lottery/lotto')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/lottery/lotto')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
-describe.skip('vikingalotto', function () {
+describe.skip('vikingalotto', () => {
     // The only thing that changes is the form attribute, so why not just re-use the object
-  var fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
+  const fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
 
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/lottery/vikingalotto')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/lottery/vikingalotto')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
-describe.skip('eurojackpot', function () {
+describe.skip('eurojackpot', () => {
     // The only thing that changes is the form attribute, so why not just re-use the object
-  var fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
+  const fieldsToCheckFor = ['date', 'lotto', 'joker', 'prize', 'link']
 
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/lottery/eurojackpot')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/lottery/eurojackpot')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
