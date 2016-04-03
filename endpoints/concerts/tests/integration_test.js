cc0663ac208ccb0fccac3c9247981899bc7c392c
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 19:03:23 +0000

    Fix linting on concerts endpoint tests

--- endpoints/concerts/tests/integration_test.js
+++ endpoints/concerts/tests/integration_test.js
@@ -1,15 +1,14 @@
-var request = require('request')
-var assert = require('assert')
-var helpers = require('../../../lib/test_helpers.js')
+import request from 'request'
+import helpers from '../../../lib/test_helpers.js'
 
-describe('concerts', function () {
-  var fieldsToCheckFor = [
+describe('concerts', () => {
+  const fieldsToCheckFor = [
     'eventDateName', 'name', 'dateOfShow', 'userGroupName', 'eventHallName',
-    'imageSource'
+    'imageSource',
   ]
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/concerts')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/concerts')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
