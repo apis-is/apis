c9f12f8e54c09482fc9be916e31a13928f1a3f6f
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 18:50:41 +0000

    Linting carparks endpoint

--- endpoints/carparks/index.js
+++ endpoints/carparks/index.js
@@ -1,53 +1,57 @@
-var request = require('request')
-var $ = require('cheerio')
-var h = require('apis-helpers')
-var app = require('../../server')
+import request from 'request'
+import $ from 'cheerio'
+import h from 'apis-helpers'
+import app from '../../server'
 
-app.get('/carparks', function (req, res) {
-
-  var url = 'http://www.bilastaedasjodur.is/'
+app.get('/carparks', (req, res) => {
+  const url = 'http://www.bilastaedasjodur.is/'
 
   request.get({
     headers: { 'User-Agent': h.browser() },
-    url: url
-  }, function (error, response, body) {
-    if (error || response.statusCode !== 200)
-      return res.status(500).json({ error:'www.bilastaedasjodur.is refuses to respond or give back data' })
-
-    try {
-      var data = $(body)
-    } catch (error) {
-      return res.status(500).json({ error:'Could not parse body' })
+    url,
+  }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      return res.status(500).json({
+        error: 'www.bilastaedasjodur.is refuses to respond or give back data',
+      })
     }
 
-    var obj = { results: [] }
-    var hus = $('.hus', $(body))
+    try {
+      // FIXME: What is the point of this? It doesn't seem to be used anywhere
+      // eslint-disable-next-line no-unused-vars
+      const data = $(body)
+    } catch (exc) {
+      return res.status(500).json({ error: 'Could not parse body' })
+    }
 
-    var js = body.match(/LatLng\((.*?)\)/g)
+    const obj = { results: [] }
+    const hus = $('.hus', $(body))
+
+    const js = body.match(/LatLng\((.*?)\)/g)
 
     function parseCoordinates(str) {
       try {
-        var Regexp = /(?:^|\s)LatLng.(.*?)\)(?:\s|$)/g
-        var match = Regexp.exec(str)
+        const Regexp = /(?:^|\s)LatLng.(.*?)\)(?:\s|$)/g
+        const match = Regexp.exec(str)
         return match[1].split(', ')
-      } catch (error) {
+      } catch (exc) {
         return null
       }
     }
 
-    for (var i = 0; i < hus.length; i++) {
-      var that = hus[i]
-      var freeSpaces = parseInt($(that).find('.ib.free h1').text())
-      var totalSpaces = parseInt($(that).find('.ib.total h1').text())
+    for (let i = 0; i < hus.length; i++) {
+      const that = hus[i]
+      const freeSpaces = parseInt($(that).find('.ib.free h1').text(), 10)
+      const totalSpaces = parseInt($(that).find('.ib.total h1').text(), 10)
 
       obj.results.push({
         name: $(that).find('aside h2').text(),
         address: $(that).find('h5').text(),
         parkingSpaces: {
           free: !isNaN(freeSpaces) ? freeSpaces : null,
-          total: !isNaN(totalSpaces) ? totalSpaces : null
+          total: !isNaN(totalSpaces) ? totalSpaces : null,
         },
-        coordinates: parseCoordinates(js[i])
+        coordinates: parseCoordinates(js[i]),
       })
     }
 
--- endpoints/carparks/tests/integration_test.js
+++ endpoints/carparks/tests/integration_test.js
@@ -1,12 +1,11 @@
-var request = require('request')
-var assert = require('assert')
-var helpers = require('../../../lib/test_helpers.js')
+import request from 'request'
+import helpers from '../../../lib/test_helpers.js'
 
-describe('carparks', function () {
-  it('should return an array of objects containing correct fields', function (done) {
-    var fieldsToCheckFor = ['name', 'address', 'parkingSpaces', 'coordinates']
-    var params = helpers.testRequestParams('/carparks')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+describe('carparks', () => {
+  it('should return an array of objects containing correct fields', (done) => {
+    const fieldsToCheckFor = ['name', 'address', 'parkingSpaces', 'coordinates']
+    const params = helpers.testRequestParams('/carparks')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
   })
 })
