fb39bf907f94cb0023a9e12c9dd4bf602af18b71
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 19:41:33 +0000

    Fix linting for earthquake endpoint

--- endpoints/earthquake/index.js
+++ endpoints/earthquake/index.js
@@ -1,47 +1,22 @@
-var request = require('request')
-var cheerio = require('cheerio')
-var browser = require('apis-helpers').browser
-var app = require('../../server')
-
-/*
- * Hraun table parse
- */
-app.get('/earthquake/is', function (req, res, next) {
-  getEarthquakes(function (error, body) {
-    if (error) return res.status(500).json({ error:error.toString() })
-
-    return res.json({ results: parseList(body) })
-  })
-})
-
-/*
- * Main vedur.is website (JS variable included in the source) (secondary source of information).
- */
-app.get('/earthquake/is/sec', function (req, res, next) {
-  getEarthquakes(function (error, body) {
-    if (error) return res.json({ error:error.toString() })
-    return res.json({
-      results: parseJavaScriptVariable(body)
-    })
-  }, {
-    url: 'http://www.vedur.is/skjalftar-og-eldgos/jardskjalftar',
-    headers: { 'User-Agent': browser() },
-    encoding: 'utf-8' // needed for some reason.. defaulting to ISO-8859-1
-  })
-})
+import request from 'request'
+import cheerio from 'cheerio'
+import helpers from 'apis-helpers'
+import app from '../../server'
+const browser = helpers.browser
 
 /*
    This function only handles the request part and calls callback with
    the body
    */
 function getEarthquakes(callback, params) {
-  var req_params = (params == null) ? {
+  const reqParams = (params === null) ? {
     url: 'http://hraun.vedur.is/ja/skjalftar/skjlisti.html',
     headers: { 'User-Agent': browser() },
-    encoding: 'binary' // needed for some reason.. defaulting to ISO-8859-1
+    // needed for some reason.. defaulting to ISO-8859-1
+    encoding: 'binary',
   } : params
 
-  request(req_params, function (error, res, body) {
+  request(reqParams, (error, res, body) => {
     if (error || res.statusCode !== 200) {
       return callback(new Error('Could not retrieve the data from the data source'))
     }
@@ -56,9 +31,11 @@ function getEarthquakes(callback, params) {
  * Note that it is synchronous.
  */
 function parseJavaScriptVariable(body) {
-  var jsonString = '' // Work with empty string if scraping fails.
+  // Work with empty string if scraping fails.
+  let res
+  let jsonString = ''
   // Create a cheerio object from response body.
-  var $ = cheerio.load(body)
+  const $ = cheerio.load(body)
 
   // Find the variable inside one of the <script> elements in the response body.
   $('script').each(function (i, elem) {
@@ -68,36 +45,42 @@ function parseJavaScriptVariable(body) {
   })
 
   // Convert the variable to JavaScript Object Notation and fix seperators in values.
-  var res_string = jsonString.replace(/(:\'[-0-9][0-9]*)(,)([0-9]*)/g, '$1.$3')
+  const resString = jsonString.replace(/(:\'[-0-9][0-9]*)(,)([0-9]*)/g, '$1.$3')
 
   // Create a Regular expression and change date representation.
-  var regex_date = /(\'t\':)new Date\((([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+))\)(,\'a\')/g
-  var dateReplace = function (match, p1, p2, p3, p4, p5, p6, p7, p8, p9, offset, stringg) {
-    var parsed_date = new Date(parseInt(p3), parseInt(p4.split('-')[0]) - 1, parseInt(p5), parseInt(p6), parseInt(p7), parseInt(p8))
-    return p1 + '\'' + parsed_date.toISOString() + '\'' + p9
+  const regexDate = /(\'t\':)new Date\((([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+),([0-9.,-]+))\)(,\'a\')/g
+  const dateReplace = (match, p1, p2, p3, p4, p5, p6, p7, p8, p9) => {
+    const parsedDate = new Date(
+      parseInt(p3, 10),
+      parseInt(p4.split('-')[0], 10) - 1, parseInt(p5, 10),
+      parseInt(p6, 10),
+      parseInt(p7, 10),
+      parseInt(p8, 10)
+    )
+    return `${p1}\\${parsedDate.toISOString()}\\${p9}`
   }
 
   try {
     // Create semi-final JSON string.
-    var res = JSON.parse(res_string.replace(regex_date, dateReplace).replace(/\'/g, '"'))
+    res = JSON.parse(resString.replace(regexDate, dateReplace).replace(/\'/g, '"'))
   } catch (ex) {
-    return JSON.parse([{ error:'Error parsing source.' }])
+    return JSON.parse([{ error: 'Error parsing source.' }])
   }
   // rename fields to match current specs
-  var res_fields = []
-  res.forEach(function (element) {
-    var tmpRow = {}
-    tmpRow['timestamp'] = element.t
-    tmpRow['latitude'] = element.lat
-    tmpRow['longitude'] = element.lon
-    tmpRow['depth'] = element.dep
-    tmpRow['size'] = element.s
-    tmpRow['quality'] = element.q
-    tmpRow['humanReadableLocation'] = element.dL + ' km ' + element.dD + ' af ' + element.dR
-    res_fields.push(tmpRow)
+  const resFields = []
+  res.forEach((element) => {
+    const tmpRow = {}
+    tmpRow.timestamp = element.t
+    tmpRow.latitude = element.lat
+    tmpRow.longitude = element.lon
+    tmpRow.depth = element.dep
+    tmpRow.size = element.s
+    tmpRow.quality = element.q
+    tmpRow.humanReadableLocation = `${element.dL} km ${element.dD} af ${element.dR}`
+    resFields.push(tmpRow)
   })
 
-  return res_fields
+  return resFields
 }
 
 /*
@@ -109,11 +92,11 @@ function parseJavaScriptVariable(body) {
    it's supposed to look like)
    */
 function parseList(body) {
-  var $ = cheerio.load(body)
+  const $ = cheerio.load(body)
 
   // currying of a function that parses a <td> with an icelandic number as a JS Number
   // used for the fieldsParser below
-  var numberField = function (i) {
+  const numberField = function (i) {
     return function ($children) {
       return + $children.eq(i).text().replace(',', '.')
     }
@@ -122,10 +105,10 @@ function parseList(body) {
   // for each row, all of this object's functions will be called to popoulate each
   // key in the generated object - they are all passed in the full array of
   // children (which are the cheerioed <td>s)
-  var fieldsParser = {
-    timestamp: function ($children) {
+  const fieldsParser = {
+    timestamp: ($children) => {
       // the fields are in this format: <td><a href>2015-12-30</a></td><td>21:31:24,0</td>
-      var date = $children.eq(0).text() + ' ' + $children.eq(1).text()
+      const date = `${$children.eq(0).text()} ${$children.eq(1).text()}`
       return new Date(date.split(',')[0])
     },
     latitude: numberField(2),
@@ -133,26 +116,61 @@ function parseList(body) {
     depth: numberField(4),
     size: numberField(5),
     quality: numberField(6),
-    humanReadableLocation: function ($children) {
+    humanReadableLocation: ($children) => {
       // these are three <td> elements' text values combined.. there migth be a
       // cleaner way to do this, but I don't know how.
-      var strObj = $children.slice(7, 10).map(function () { return $(this).text() })
+      const strObj = $children.slice(7, 10).map(function () { return $(this).text() })
       return Array.prototype.join.call(strObj, ' ').replace(/^\s*|\s*$/g, '')
-    }
+    },
   }
 
   // This finds all the table rows in the list, except the first row, which
   // is a header row.
   const data = []
 
-  $('table').eq(2).find('tr').slice(1).map(function () {
-    var $children = $(this).children(),
-      obj = {}
-    for (var key in fieldsParser) {
-      obj[key] = fieldsParser[key]($children)
+  $('table').eq(2).find('tr').slice(1).map(() => {
+    const $children = $(this).children()
+    const obj = {}
+    for (const key in fieldsParser) {
+      if (fieldsParser.hasOwnProperty(key)) {
+        obj[key] = fieldsParser[key]($children)
+      }
     }
     data.push(obj)
+    return obj
   })
 
   return data
 }
+
+/*
+ * Hraun table parse
+ */
+app.get('/earthquake/is', (req, res) => {
+  getEarthquakes((error, body) => {
+    if (error) {
+      return res.status(500).json({ error: error.toString() })
+    }
+
+    return res.json({ results: parseList(body) })
+  })
+})
+
+/*
+ * Main vedur.is website (JS variable included in the source) (secondary source of information).
+ */
+app.get('/earthquake/is/sec', (req, res) => {
+  getEarthquakes((error, body) => {
+    if (error) {
+      return res.json({ error: error.toString() })
+    }
+    return res.json({
+      results: parseJavaScriptVariable(body),
+    })
+  }, {
+    url: 'http://www.vedur.is/skjalftar-og-eldgos/jardskjalftar',
+    headers: { 'User-Agent': browser() },
+    // needed for some reason.. defaulting to ISO-8859-1
+    encoding: 'utf-8',
+  })
+})
