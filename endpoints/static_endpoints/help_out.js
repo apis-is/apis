3cbc1b755fb0df03ebb6a7cd4580e9e92fc1af40
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 20:42:30 +0000

    Fix linting for static endpoints

--- endpoints/static_endpoints/help_out.js
+++ endpoints/static_endpoints/help_out.js
@@ -1,13 +1,14 @@
+
+function helpOut(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.status(200).json({ message: 'Send us mail: apis@apis.is ,thanks for your interest!' })
+  next()
+}
+
 exports.setup = function (server) {
   server.post({ path: '/help_out', version: '1.0.0' }, helpOut)
   server.get({ path: '/help_out', version: '1.0.0' }, helpOut)
   server.post({ path: '/help', version: '1.0.0' }, helpOut)
   server.get({ path: '/help', version: '1.0.0' }, helpOut)
-}
-
-var helpOut = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.status(200).json({ message:'Send us mail: apis@apis.is ,thanks for your interest!' })
-  next()
 }
