d0c280b5f21df587732e05cdd4438a20f458a43e
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 20:02:14 +0000

    Fix linting for hospital endpoint tests

--- endpoints/hospital/tests/integration_test.js
+++ endpoints/hospital/tests/integration_test.js
@@ -1,17 +1,16 @@
-var request = require('request')
-var assert = require('assert')
-var helpers = require('../../../lib/test_helpers.js')
+import request from 'request'
+import helpers from '../../../lib/test_helpers.js'
 
-describe('hospital', function () {
+describe('hospital', () => {
     // The only thing that changes is the form attribute, so why not just re-use the object
-  var fieldsToCheckFor = ['birthNumbers', 'surgeries', 'dischargedNumbers',
-			    'hospitalizedNumbers', 'atwork', 'patients-child',
-		            'patients-er', 'patients-walk', 'patients-icu', 'donors', 'patients-skilun',
- 			    'patients-heart2']
+  const fieldsToCheckFor = [
+    'birthNumbers', 'surgeries', 'dischargedNumbers', 'hospitalizedNumbers', 'atwork', 'patients-child',
+    'patients-er', 'patients-walk', 'patients-icu', 'donors', 'patients-skilun', 'patients-heart2',
+  ]
 
-  it('should return an array of objects containing correct fields', function (done) {
-    var params = helpers.testRequestParams('/hospital')
-    var resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+  it('should return an array of objects containing correct fields', (done) => {
+    const params = helpers.testRequestParams('/hospital')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request(params, resultHandler)
   })
 })
