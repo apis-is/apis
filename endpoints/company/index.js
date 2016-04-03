360a111d0783d605e86c9de817e78c3dd2992ff2
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-04-03 19:01:57 +0000

    Fix linting on company endpoint

--- endpoints/company/index.js
+++ endpoints/company/index.js
@@ -1,56 +1,53 @@
-var request = require('request')
-var cheerio = require('cheerio')
-var h = require('apis-helpers')
-var app = require('../../server')
+import request from 'request'
+import cheerio from 'cheerio'
+import h from 'apis-helpers'
+import app from '../../server'
 
-app.get('/company', function (req, res, next) {
-
-  var queryString = {
+app.get('/company', (req, res) => {
+  const queryString = {
     nafn: req.query.name || '',
     heimili: req.query.address || '',
     kt: req.query.socialnumber || '',
-    vsknr: req.query.vsknr || ''
+    vsknr: req.query.vsknr || '',
   }
 
   request.get({
     headers: { 'User-Agent': h.browser() },
     url: 'http://www.rsk.is/fyrirtaekjaskra/leit',
-    qs: queryString
-  }, function (error, response, body) {
+    qs: queryString,
+  }, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      return res.status(500).json({ error:'www.rsk.is refuses to respond or give back data' })
+      return res.status(500).json({ error: 'www.rsk.is refuses to respond or give back data' })
     }
 
-    var obj = { results: [] },
-      $ = cheerio.load(body, { decodeEntities: false })
+    const obj = { results: [] }
+    const $ = cheerio.load(body, { decodeEntities: false })
 
     if ($('.resultnote').length === 0) {
-      var tr = $('.boxbody > .nozebra tbody tr').first()
+      const tr = $('.boxbody > .nozebra tbody tr').first()
       if (tr.length > 0) {
-        var name = $('.boxbody > h1').html(),
-          sn = $('.boxbody > h1').html()
+        const name = $('.boxbody > h1').html()
+        const sn = $('.boxbody > h1').html()
 
         obj.results.push({
           name: name.substring(0, name.indexOf('(') - 1),
           sn: sn.substring(sn.length - 11, sn.length - 1),
           active: $('p.highlight').text().length === 0 ? 1 : 0,
-          address: tr.find('td').eq(0).text()
+          address: tr.find('td').eq(0).text(),
         })
       }
     } else {
-      $('table tr').slice(1).each(function () {
-
-        var td = $(this).find('td')
-        var nameRoot = td.eq(1).text()
-        var felagAfskrad = '(Félag afskráð)'
+      $('table tr').slice(1).each(() => {
+        const td = $(this).find('td')
+        const nameRoot = td.eq(1).text()
+        const felagAfskrad = '(Félag afskráð)'
 
         obj.results.push({
           name: nameRoot.replace('\n', '').replace(felagAfskrad, '').replace(/^\s\s*/, '').replace(/\s\s*$/, ''),
           sn: td.eq(0).text(),
           active: nameRoot.indexOf(felagAfskrad) > -1 ? 0 : 1,
-          address: td.eq(2).text()
+          address: td.eq(2).text(),
         })
-
       })
     }
 
