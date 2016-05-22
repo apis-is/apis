0fb1dfcdb30f418fbd06a320b0d79424dcc8d35e
Sigurður Snær Eiríksson <sse1987@gmail.com>
2016-05-22 10:57:44 +0000

    Fixed eslint problems

--- endpoints/isnic/index.js
+++ endpoints/isnic/index.js
@@ -5,7 +5,7 @@ import app from '../../server'
 
 
 app.get('/isnic', (req, res) => {
-  let domainName = req.query.domain || ''
+  const domainName = req.query.domain || ''
 
   if (!domainName) {
     return res.status(431).json({ error: 'Please provide a valid domainName to lookup' })
@@ -35,22 +35,22 @@ app.get('/isnic', (req, res) => {
       const label = $(this).find('label').text().trim()
       const val = $(this).text().replace(label, '').trim()
 
-      fields.push({val:val,label:label})
+      fields.push({ val, label })
     })
 
     if (fields.length > 0) {
       obj.results.push({
-        domain:         (fields.find(x => x.label === 'Domain:')            || {val:''}).val,
-        registrantname: (fields.find(x => x.label === 'Registrant name:')   || {val:''}).val,
-        address:        (fields.find(x => x.label === 'Address:')           || {val:''}).val,
-        city:           (fields.find(x => x.label === 'City/Municipality:') || {val:''}).val,
-        postalCode:     (fields.find(x => x.label === 'Postal code:')       || {val:''}).val,
-        country:        (fields.find(x => x.label === 'Country:')           || {val:''}).val,
-        phone:          (fields.find(x => x.label === 'Phone:')             || {val:''}).val,
-        email:          (fields.find(x => x.label === 'E-mail:')            || {val:''}).val,
-        registered:     (fields.find(x => x.label === 'Registered:')        || {val:''}).val,
-        expires:        (fields.find(x => x.label === 'Expires:')           || {val:''}).val,
-        lastChange:     (fields.find(x => x.label === 'Last change:')       || {val:''}).val,
+        domain: (fields.find(x => x.label === 'Domain:') || { val: '' }).val,
+        registrantname: (fields.find(x => x.label === 'Registrant name:') || { val: '' }).val,
+        address: (fields.find(x => x.label === 'Address:') || { val: '' }).val,
+        city: (fields.find(x => x.label === 'City/Municipality:') || { val: '' }).val,
+        postalCode: (fields.find(x => x.label === 'Postal code:') || { val: '' }).val,
+        country: (fields.find(x => x.label === 'Country:') || { val: '' }).val,
+        phone: (fields.find(x => x.label === 'Phone:') || { val: '' }).val,
+        email: (fields.find(x => x.label === 'E-mail:') || { val: '' }).val,
+        registered: (fields.find(x => x.label === 'Registered:') || { val: '' }).val,
+        expires: (fields.find(x => x.label === 'Expires:') || { val: '' }).val,
+        lastChange: (fields.find(x => x.label === 'Last change:') || { val: '' }).val,
       })
     }
 
