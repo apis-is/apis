50368cc525e463fa20aa10bc35a411135ba6f361
Loknar <svefgud@gmail.com>
2016-06-01 22:45:36 +0000

    fix all the eslint things :(

--- endpoints/petrol/index.js
+++ endpoints/petrol/index.js
@@ -29,11 +29,11 @@ function queryTimestamps(callback) {
     Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.8,is;q=0.6',
     'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
+    Connection: 'keep-alive',
     Host: 'gist.github.com',
     Pragma: 'no-cache',
     'Upgrade-Insecure-Requests': 1,
-    'User-Agent': h.browser()
+    'User-Agent': h.browser(),
   }
   request.get({
     headers,
@@ -49,36 +49,36 @@ function queryTimestamps(callback) {
 function parseTimestamps(htmlText) {
   const data = $(htmlText)
   return {
-    'last_pricechanges': data.find('#file-prices_changed_timestamp-LC1').text().slice(0, 23),
-    'last_pricecheck': data.find('#file-prices_lookup_timestamp-LC1').text().slice(0, 23)
+    lastPriceChanges: data.find('#file-prices_changed_timestamp-LC1').text().slice(0, 23),
+    lastPriceCheck: data.find('#file-prices_lookup_timestamp-LC1').text().slice(0, 23),
   }
 }
 
 app.get('/petrol', (req, res) => {
-  const timestamp_apis = (new Date().toISOString().slice(0, 23))
-  const nine_minutes = 900
-  queryData((error, response, jsonText) => {
-    if (error || response.statusCode !== 200) {
+  const timestampApis = (new Date().toISOString().slice(0, 23))
+  const nineMinutes = 900
+  queryData((error1, response1, jsonText) => {
+    if (error1 || response1.statusCode !== 200) {
       return res.status(500).json({
         error: 'raw.githubusercontent.com refuses to respond or give back data',
       })
     }
     const results = JSON.parse(jsonText).stations
-    queryTimestamps((error, response, htmlText) => {
-      if (error || response.statusCode !== 200) {
+    queryTimestamps((error2, response2, htmlText) => {
+      if (error2 || response2.statusCode !== 200) {
         return res.cache(nine_minutes).json({
           results,
-          timestamp_apis,
-          timestamp_pricechanges: null,
-          timestamp_pricecheck: null,
+          timestampApis,
+          timestampPriceChanges: null,
+          timestampPriceCheck: null,
         })
       }
       const timestamps = parseTimestamps(htmlText)
       return res.cache(nine_minutes).json({
         results,
-        timestamp_apis,
-        timestamp_last_pricechanges: timestamps.last_pricechanges,
-        timestamp_last_pricecheck: timestamps.last_pricecheck,
+        timestampApis,
+        timestampPriceChanges: timestamps.lastPriceChanges,
+        timestampPriceCheck: timestamps.lastPriceCheck,
       })
     })
   })
