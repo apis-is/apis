232a1043b6eb095d479d9c77a1e64a676f2d9cd3
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-07-25 01:48:46 +0000

    Add integration test for address graphql query

--- endpoints/address/tests/integration_test.js
+++ endpoints/address/tests/integration_test.js
@@ -1,5 +1,6 @@
 import request from 'request'
 import helpers from '../../../lib/test_helpers'
+import assert from 'assert'
 
 describe('zip', () => {
   it('should return an array of objects containing correct fields', (done) => {
@@ -7,5 +8,29 @@ describe('zip', () => {
     const params = helpers.testRequestParams('/address', { address: 'laugavegur 26' })
     const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
+  })
+})
+
+describe('address graphql', () => {
+  it('should return only the field that were asked for', (done) => {
+    const expected = {
+      data: {
+        address: [
+          { street: 'laugavegur', house: '1' },
+        ],
+      },
+    }
+    const params = helpers.testRequestParams('/graphql', { query: `
+      query {
+        address(address: "laugavegur 1") {
+          street
+          house
+        }
+      }
+    ` })
+    request.get(params, (err, res, body) => {
+      assert(body === JSON.stringify(expected), 'result will match expected')
+      done()
+    })
   })
 })
