51b56034358418312ab9a083df4c7b916caa0477
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-07-26 23:29:04 +0000

    Add descriptions to the fields of the GraphQL Address type

--- endpoints/address/graphql_schema.js
+++ endpoints/address/graphql_schema.js
@@ -9,12 +9,29 @@ import lookupAddresses from './index'
 const addressType = new GraphQLObjectType({
   name: 'Address',
   fields: {
-    street: { type: GraphQLString },
-    house: { type: GraphQLString },
-    zip: { type: GraphQLString },
-    city: { type: GraphQLString },
-    apartment: { type: GraphQLString },
-    letter: { type: GraphQLString },
+    street: {
+      type: GraphQLString,
+      description: 'The name of the street the address belongs to',
+    },
+    house: {
+      type: GraphQLString,
+      description: 'The house number the address belongs to',
+    },
+    zip: {
+      type: GraphQLString,
+      description: 'The zip code where this address is registered',
+    },
+    city: {
+      type: GraphQLString,
+      description: 'The city where this address is registered',
+    },
+    apartment: {
+      type: GraphQLString,
+      description: 'A name for an apartment belonging to this address',
+    },
+    letter: {
+      type: GraphQLString
+    },
   },
 })
 
