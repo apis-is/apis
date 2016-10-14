5904178dac4addd2f5caf08a9645f541d0bda1b3
Kristján Oddsson <koddsson@gmail.com>
2016-10-14 09:31:04 -0700

    Temporarily remove graphql endpoint

--- endpoints/graphql/index.js
+++
@@ -1,9 +0,0 @@
-import graphqlHTTP from 'express-graphql'
-import bodyParser from 'body-parser'
-import schema from './schema'
-import app from '../../server'
-
-app.use('/graphql', bodyParser.json(), graphqlHTTP({
-  schema,
-  graphiql: true,
-}))
