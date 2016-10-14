e142805f50642252d1b3c4896860da77ddd97aaf
Kristján Oddsson <koddsson@gmail.com>
2016-10-14 10:16:21 -0700

    Remove cinema tests

--- endpoints/cinema/tests/integration_test.js
+++ endpoints/cinema/tests/integration_test.js
@@ -1,22 +1,5 @@
 import request from 'request'
 import helpers from '../../../lib/test_helpers.js'
 
-describe('cinema', () => {
-  const fieldsToCheckFor = ['image', 'imdb', 'released', 'restricted', 'showtimes', 'title']
-
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/cinema')
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request(params, resultHandler)
-  })
-})
-
-describe('cinema theaters', () => {
-  const fieldsToCheckFor = ['image', 'name', 'location', 'movies']
-
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/cinema/theaters')
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request(params, resultHandler)
-  })
-})
+describe('cinema', () => { })
+describe('cinema theaters', () => { })
