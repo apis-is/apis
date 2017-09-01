82c92e52a9981d5ec98d382626de53025111fb77
Gunnar Sturla Ágústuson <gsa@arktech.net>
2017-09-01 11:33:25 +0000

    /ship endpoint working properly

--- endpoints/ship/index.js
+++ endpoints/ship/index.js
@@ -3,6 +3,8 @@ import $ from 'cheerio'
 import h from 'apis-helpers'
 import app from '../../server'
 
+const parseIsFloat = (str) => parseFloat(str.replace('.', '').replace(',', '.'))
+
 const lookupShip = name => new Promise((resolve, reject) => {
   // Encode shipName so that Icelandic characters will work
   const shipName = encodeURIComponent(name)
@@ -17,28 +19,88 @@ const lookupShip = name => new Promise((resolve, reject) => {
     }
 
     const data = $(body)
-    const fields = []
+    const typeDict = {
+      BJÖRGUNARSKIP: 'LIFEBOAT',
+      DRÁTTARSKIP: 'TUGBOAT',
+      'DÝPK. OG SANDSKIP': 'DREDGERSET',
+      DÝPKUNARSKIP: 'DREDGER',
+      'EFTIRLITS‐ OG BJÖRGUNARSKIP': 'PATROL AND LIFEBOAT',
+      FARÞEGASKIP: 'PASSENGERSHIP',
+      'FISKI,FARÞEGASKIP': 'FISHING,PASSENGERSHIP',
+      FISKISKIP: 'FISHING VESSEL',
+      FLOTBRYGGJA: 'PONTOON BRIDGE',
+      FLOTKVÍ: 'FLOATING DOCK',
+      'FLUTNINGA/BRUNNSKIP': 'CARGO VESSEL LIVE FISH CARRIER',
+      FRÍSTUNDAFISKISKIP: '',
+      'HAFNSÖGU/DRÁTTARSKIP': 'PILOT‐ AND TUGBOAT',
+      HVALVEIÐISKIP: 'FISH.V.WHALE CATCHER',
+      LÓÐSSKIP: 'PILOT BOAT',
+      'NÓTAVEIÐI/SKUTTOGARI': 'FISH.V.PURSE STEINERS/ST',
+      OLÍUSKIP: 'OIL TANKER',
+      PRAMMI: 'BARGE',
+      RANNSÓKNARSKIP: 'RESEARCH VESSEL',
+      SAFNSKIP: 'MUSEUM SHIP',
+      SEGLSKIP: 'SAILBOAT',
+      SJÓMÆLINGASKIP: '',
+      SKEMMTISKIP: 'PLEASURE CRAFT',
+      SKÓLASKIP: 'TRAINING VESSEL',
+      SKUTTOGARI: 'FISH.V.STERN TRAWLER',
+      VARÐSKIP: 'INSPECTION SHIP',
+      VINNUSKIP: 'WORKBOAT',
+      VÍKINGASKIP: 'VIKING SHIP',
+      VÖRUFLUTNINGASKIP: 'DRY CARGO SHIP',
+      ÞANGSKURÐARPRAMMI: 'BARGE',
+    }
+    // Translations from: https:// www.samgongustofa.is/media/siglingar/skip/Vefskipaskra-2012.pdf
+    // https://www.iho.int/mtg_docs/com_wg/CSBWG/CSBWG2/CSBWG2-5.2.2-Ship_Type_Identification_Recommendations.pdf
 
-    data.find('.vehicleinfo ul li').each(function () {
-      const val = $(this).find('span').text()
-      fields.push(val)
+    const fieldList = []
+    data.find('.vehicleinfo ul').each((index, element) => {
+      const fields = []
+      $(element).find('li').each((i, el) => {
+        if (i !== 10) {
+          const val = $(el).find('span').text()
+          fields.push(val)
+        } else {
+          // We'll treat the owners' field specifically
+          const owners = []
+          $(el).children('span').each(function () {
+            const info = $(this).text().split(/\s{2,}/g)
+            const owner = {
+              name: info[1],
+              socialnumber: info[2].replace('(kt. ', '').replace('-', '').replace(')', ''),
+              share: parseIsFloat(info[3].replace(' eign', '')) / 100,
+            }
+            owners.push(owner)
+          })
+          fields.push(owners)
+        }
+      })
+      if (fields.length > 0) {
+        fieldList.push(fields)
+      }
     })
 
-    if (fields.length > 0) {
-      const registrationStatus = fields[5] === 'Skráð' ? 'Listed' : 'Unlisted';
-      resolve({
-        name: fields[0],
-        type: fields[1],
-        registrationNumber: fields[2],
-        regionalCode: fields[3],
-        homePort: fields[4],
-        registrationStatus,
-        grossTonnage: fields[6],
-        bruttoTonn: fields[7],
-        length: fields[8],
-        build: fields[9],
-        owners: fields[10],
-      })
+
+    if (fieldList.length > 0 && fieldList[0].length > 0) {
+      resolve(fieldList.map(fields => {
+        const type = typeDict[fields[1]] ? typeDict[fields[1]] : fields[1]
+        const registrationStatus = fields[5] === 'Skráð' ? 'Listed' : 'Unlisted'
+        return {
+          name: fields[0],
+          type,
+          registrationNumber: fields[2],
+          regionalCode: fields[3].replace(/(\(.*\))/g, '').match(/(\S.*)/g).join(' '),
+          homePort: fields[4],
+          registrationStatus,
+          grossRegisterTonnage: parseIsFloat(fields[6]),
+          grossTonnage: parseIsFloat(fields[7]),
+          length: parseIsFloat(fields[8]),
+          buildYear: parseInt(fields[9].split('af')[0], 10),
+          buildYard: fields[9].split('af')[1].match(/(\S.*)/g).join(' '),
+          owners: fields[10],
+        }
+      }))
     } else {
       reject(`No ship found with the name ${name}`)
     }
@@ -46,14 +108,14 @@ const lookupShip = name => new Promise((resolve, reject) => {
 })
 
 app.get('/ship', (req, res) => {
-  const shipName = req.query.name || req.query.shipName || ''
+  const search = req.query.search || ''
 
-  if (!shipName) {
-    return res.status(431).json({ error: 'Please provide a valid shipName to lookup' })
+  if (!search) {
+    return res.status(431).json({ error: 'Please provide a valid search string to lookup' })
   }
 
-  lookupShip(shipName)
-    .then(ship => res.cache().json({ results: [ship] }))
+  lookupShip(search)
+    .then(ships => res.cache().json({ results: ships }))
     .catch(error => res.status(500).json({ error }))
 })
 
