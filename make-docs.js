11882468bf8a89bf9f2b3ce5f9a6cf5f94b760e3
Kristján Oddsson <koddsson@gmail.com>
2017-10-12 23:18:05 +0100

    chore: clean up documentation folder structure

--- make-docs.js
+++ make-docs.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const globby = require('globby')
 const marked = require('marked')
 
-globby(['**/*.md', '!node_modules/**', '!README.md']).then(paths => {
-  let content = fs.readFileSync('./docs/header.html', 'utf8')
+globby(['./docs/*.md', './endpoints/**/*.md', '!node_modules/**']).then(paths => {
+  let content = ''
   paths.forEach(path => {
-    content += fs.readFileSync(path)
+    content += fs.readFileSync(path, 'utf8')
   })
   const html = marked(content, { escapeMarkdown: false })
   fs.writeFileSync('./docs/dist/index.html', html, 'utf8')
