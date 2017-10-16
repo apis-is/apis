0fa73e5503e3469e9af05736e9e61359983f582d
Kristján Oddsson <koddsson@github.com>
2017-10-16 07:45:57 +0100

    Fix 392 (#393)
    * fix: don't add a instance if there's no image
    
    * chore: re-record test mock data
    
    * chore(lint): fix lint errors

--- endpoints/cinema/tests/integration_test.js
+++ endpoints/cinema/tests/integration_test.js
@@ -1,2 +1,12 @@
-describe('cinema', () => { })
+const request = require('request')
+const helpers = require('../../../lib/test_helpers')
+
+describe('/cinema', () => {
+  it('should return an array of objects containing correct fields', (done) => {
+    const fieldsToCheckFor = ['title', 'released', 'restricted', 'imdb', 'imdbLink', 'image', 'showtimes']
+    const params = helpers.testRequestParams('/cinema')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+    request.get(params, resultHandler)
+  })
+})
 describe('cinema theaters', () => { })
