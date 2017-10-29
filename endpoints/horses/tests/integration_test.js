83cece71b60d2409067295f0536671892168fc83
Kristján Oddsson <koddsson@gmail.com>
2017-10-29 20:20:49 +0100

    chore(horses): wrap tests in a `describe()`

--- endpoints/horses/tests/integration_test.js
+++ endpoints/horses/tests/integration_test.js
@@ -15,38 +15,40 @@ const fieldsToCheckFor = [
   'mother',
 ]
 
-describe('multi-results', function () {
-  this.timeout(20000)
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/horses', { name: 'Oddur', origin: 'Selfossi' })
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request.get(params, resultHandler)
+describe('horses', () => {
+  describe('multi-results', function () {
+    this.timeout(20000)
+    it('should return an array of objects containing correct fields', (done) => {
+      const params = helpers.testRequestParams('/horses', { name: 'Oddur', origin: 'Selfossi' })
+      const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+      request.get(params, resultHandler)
+    })
   })
-})
 
-describe('single-result-by-name', function () {
-  this.timeout(20000)
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/horses', { name: 'Fálki', origin: 'Geirshlíð' })
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request.get(params, resultHandler)
+  describe('single-result-by-name', function () {
+    this.timeout(20000)
+    it('should return an array of objects containing correct fields', (done) => {
+      const params = helpers.testRequestParams('/horses', { name: 'Fálki', origin: 'Geirshlíð' })
+      const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+      request.get(params, resultHandler)
+    })
   })
-})
 
-describe('single-result-partial-info', function () {
-  this.timeout(20000)
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/horses', { name: 'Lotta', origin: 'Ármóti' })
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request.get(params, resultHandler)
+  describe('single-result-partial-info', function () {
+    this.timeout(20000)
+    it('should return an array of objects containing correct fields', (done) => {
+      const params = helpers.testRequestParams('/horses', { name: 'Lotta', origin: 'Ármóti' })
+      const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+      request.get(params, resultHandler)
+    })
   })
-})
 
-describe('single-result-by-id', function () {
-  this.timeout(20000)
-  it('should return an array of objects containing correct fields', (done) => {
-    const params = helpers.testRequestParams('/horses', { id: 'IS1987187700' })
-    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
-    request.get(params, resultHandler)
+  describe('single-result-by-id', function () {
+    this.timeout(20000)
+    it('should return an array of objects containing correct fields', (done) => {
+      const params = helpers.testRequestParams('/horses', { id: 'IS1987187700' })
+      const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+      request.get(params, resultHandler)
+    })
   })
 })
