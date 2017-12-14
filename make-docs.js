e652ee66fea4a9d86bc79d83e8400344512a30dd
Kristján Oddsson <koddsson@gmail.com>
2017-12-14 22:05:02 +0000

    add parentheses around arrow function arguments

--- make-docs.js
+++ make-docs.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const globby = require('globby')
 const marked = require('marked')
 
-globby(['./docs/*.md', './endpoints/**/*.md', '!node_modules/**']).then(paths => {
+globby(['./docs/*.md', './endpoints/**/*.md', '!node_modules/**']).then((paths) => {
   let content = ''
-  paths.forEach(path => {
+  paths.forEach((path) => {
     content += fs.readFileSync(path, 'utf8')
   })
   const html = marked(content, { escapeMarkdown: false })
   fs.writeFileSync('./docs/dist/index.html', html, 'utf8')
-}).catch(error => {
+}).catch((error) => {
   console.error(error)
 })
