638391a17560bd179118c160fd5e8d3f47524702
Sigurður Snær Eiríksson <sse1987@gmail.com>
2018-09-25 12:33:48 +0000

    Updated test after changes for /cinema endpoint

--- endpoints/cinema/tests/integration_test.js
+++ endpoints/cinema/tests/integration_test.js
@@ -3,7 +3,7 @@ const helpers = require('../../../lib/test_helpers')
 
 describe('/cinema', () => {
   it('should return an array of objects containing correct fields', (done) => {
-    const fieldsToCheckFor = ['title', 'released', 'restricted', 'imdb', 'imdbLink', 'image', 'showtimes']
+    const fieldsToCheckFor = ['title', 'released', 'restricted', 'imdb', 'image', 'showtimes']
     const params = helpers.testRequestParams('/cinema')
     const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
