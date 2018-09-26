ac1b363bbe0caa70caa2a2e065efe97a022ff7a6
Sigurður Snær Eiríksson <sse1987@gmail.com>
2018-09-26 15:05:08 +0000

    Fixed the endpoint /cinema/theaters and also added test for it

--- endpoints/cinema/index.js
+++ endpoints/cinema/index.js
@@ -2,10 +2,110 @@ const request = require('request')
 const cheerio = require('cheerio')
 const app = require('../../server')
 
+const theatersMetaData = require('./theaters')
+
 // Utility function
 const scrapeImage = (src) => {
   const urls = src.match(/\/images\/poster\/.+\.(jpg|jpeg|png)/ig) || null
   return urls === null ? null : `http://kvikmyndir.is${urls[0]}`
+}
+
+const scrapeMovieCinemaShowtimes = ($) => {
+  // Base object to be added to and eventually sent as a JSON response.
+  const obj = {
+    results: []
+  }
+
+  // DOM elements array containing all movies.
+  const movies = $('.stimar')
+
+  // Loop through movies
+  movies.each(function () {
+    // This movie.
+    const movie = $(this)
+
+    // Showtimes for JSON
+    const showtimes = []
+
+    // Find all theaters and loop through them.
+    const theaters = movie.find('[class^="biotimar"]')
+
+    theaters.each(function () {
+      // Single theater
+      const theater = {
+        theater: $(this).find('h3').text().trim(),
+        schedule: [],
+      }
+
+      // Loop through each showtime and add them to the theater schedule.
+      $(this).find('ul.time li').each(function () {
+        // Remove all VIP badge, Icelandic badge and making the time clean
+        $(this).find('.tegund, .salur').remove()
+        theater.schedule.push($(this).text().trim())
+      })
+
+      // Add theater to showtimes array.
+      showtimes.push(theater)
+    })
+
+    const releasedYear = movie.find('.title .year').text().trim()
+    // After scraping the year, it's removed so we can scrape the title without the year in it
+    movie.find('.year').remove()
+    const movieTitle = movie.find('.title').text().trim()
+
+    const src = movie.find('img').attr('src')
+    const movieImage = src ? scrapeImage(src) : null
+
+    // Create an object of info and add it to the 'results' array.
+    obj.results.push({
+      title: movieTitle,
+      released: releasedYear,
+      restricted: movie.find('.aldur').text().trim().replace(/\s{2,}/g, ' '),
+      imdb: movie.find('.imdb-einkunn').text().trim(),
+      image: movieImage,
+      showtimes,
+    })
+  })
+
+  return obj
+}
+
+const flipMoviesToTHeaters = (obj) => {
+  // DOM elements array containing all theaters.
+  const theaters = []
+
+  obj.results.forEach((item) => {
+    item.showtimes.forEach((showtime) => {
+      const movie = {
+        title: item.title,
+        schedule: showtime.schedule
+      }
+
+      // Check if the same theater is in the array, otherwise add the theater to the array
+      const theaterIndex = theaters.findIndex(theater => 
+        theater.name === showtime.theater
+      )
+      if (theaterIndex === -1) {
+        theaters.push({
+          name: showtime.theater,
+          movies: [movie]
+        })
+      } else {
+        theaters[theaterIndex].movies.push(movie)
+      }
+    })
+  })
+
+  return {
+    results: theaters.map(theater => {
+      // Finding correct meta data and merge the objects into one
+      return Object.assign(
+        {},
+        theatersMetaData.find(item => item.name === theater.name),
+        theater
+      )
+    })
+  }
 }
 
 /**
@@ -14,75 +114,21 @@ const scrapeImage = (src) => {
  */
 app.get('/cinema', (req, res) => {
   const url = 'http://kvikmyndir.is/bio/syningatimar/'
-
   request(url, (error, response, body) => {
     if (error) {
       return res.status(500).json({ error: `${url} not responding correctly...` })
     }
 
+    // Cheerio declared and then attemted to load.
     let $
-
     try {
       $ = cheerio.load(body)
     } catch (e) {
       return res.status(500).json({ error: 'Could not load the body with cherrio.' })
     }
 
-    // Base object to be added to and eventually sent as a JSON response.
-    const obj = {
-      results: [],
-    }
-
-    // DOM elements array containing all movies.
-    const movies = $('.stimar')
-
-    // Loop through movies
-    movies.each(function () {
-      // This movie.
-      const movie = $(this)
-
-      // Showtimes for JSON
-      const showtimes = []
-
-      // Find all theaters and loop through them.
-      const theaters = movie.find('[class^="biotimar"]')
-
-      theaters.each(function () {
-        // Single theater
-        const theater = {
-          theater: $(this).find('h3').text().trim(),
-          schedule: [],
-        }
-
-        // Loop through each showtime and add them to the theater schedule.
-        $(this).find('ul.time li').each(function () {
-          // Remove all VIP badge, Icelandic badge and making the time clean
-          $(this).find('.tegund, .salur').remove()
-          theater.schedule.push($(this).text().trim())
-        })
-
-        // Add theater to showtimes array.
-        showtimes.push(theater)
-      })
-
-      const realeasedYear = movie.find('.title .year').text().trim()
-      // After scraping the year, it's removed so we can scrape the title without the year in it
-      movie.find('.year').remove()
-      const movieTitle = movie.find('.title').text().trim()
-
-      const src = movie.find('img').attr('src')
-      const movieImage = src ? scrapeImage(src) : null
-
-      // Create an object of info and add it to the 'results' array.
-      obj.results.push({
-        title: movieTitle,
-        released: realeasedYear,
-        restricted: movie.find('.aldur').text().trim().replace(/\s{2,}/g, ' '),
-        imdb: movie.find('.imdb-einkunn').text().trim(),
-        image: movieImage,
-        showtimes,
-      })
-    })
+    // Returning list of movies with theaters and showtimes
+    const obj = scrapeMovieCinemaShowtimes($)
 
     return res.cache().json(obj)
   })
@@ -93,68 +139,26 @@ app.get('/cinema', (req, res) => {
  * response - JSON: Theater data within an 'results' array.
  */
 app.get('/cinema/theaters', (req, res) => {
-  const url = 'http://kvikmyndir.is/bio/syningatimar_bio/'
-
+  const url = 'http://kvikmyndir.is/bio/syningatimar/'
   request(url, (error, response, body) => {
-    if (error) return res.status(500).json({ error: `${url} not responding correctly...` })
+    if (error) {
+      return res.status(500).json({ error: `${url} not responding correctly...` })
+    }
 
     // Cheerio declared and then attemted to load.
     let $
-
     try {
       $ = cheerio.load(body)
     } catch (e) {
       return res.status(500).json({ error: 'Could not load the body with cherrio.' })
     }
 
-    // Base object to be added to
-    // and eventually sent as a JSON response.
-    const obj = {
-      results: [],
-    }
+    // Returning list of movies with theaters and showtimes
+    const objMoviesShowtime = scrapeMovieCinemaShowtimes($)
 
-    // DOM elements array containing all theaters.
-    const theaters = $('.stimar')
+    // Flip it to list of theater with movies and showtimes
+    const objTheatersShowtime = flipMoviesToTHeaters(objMoviesShowtime)
 
-    // Loop through theaters
-    theaters.each(function () {
-      // This theater.
-      const theater = $(this)
-
-      // List of movies.
-      const movies = []
-
-      // Loop through movies.
-      theater.find('#myndbio_new').each(function () {
-        // This movie.
-        const movie = $(this)
-
-        // Time schedule.
-        const schedule = []
-
-        // Loop through each showtime on schedule today.
-        movie.find('#timi_new div').each(function () {
-          // Add time to the schedule.
-          schedule.push($(this).find('.syningartimi_item').text().trim())
-        })
-
-        // Append new movie to the list of movies.
-        movies.push({
-          title: movie.find('#bio a').text().trim(),
-          schedule,
-        })
-      })
-
-      // Create an object of info
-      // and add it to the 'results' array.
-      obj.results.push({
-        name: theater.find('#mynd_titill a').text().trim(),
-        location: theater.find('.mynd_titill_artal').text().trim().replace(/(^\()|(\)$)/g, ''),
-        image: `http://kvikmyndir.is${theater.find('.mynd_plakat img').attr('src')}`,
-        movies,
-      })
-    })
-
-    return res.cache().json(obj)
+    return res.cache().json(objTheatersShowtime)
   })
 })
--- endpoints/cinema/tests/integration_test.js
+++ endpoints/cinema/tests/integration_test.js
@@ -9,4 +9,13 @@ describe('/cinema', () => {
     request.get(params, resultHandler)
   })
 })
-describe('cinema theaters', () => { })
+describe('cinema theaters', () => {
+  it('should return an array of objects containing correct fields', (done) => {
+    const fieldsToCheckFor = ['name', 'movies']
+    // The following fields are optional and not part of scraping data
+    // ['location', 'phone', 'email', 'website', 'auditoriums', 'totalSeats', 'coordinates']
+    const params = helpers.testRequestParams('/cinema/theaters')
+    const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
+    request.get(params, resultHandler)
+  })
+})
