3e9a8f97bb01bd441a4a90d984d40f1c791193e9
Kristjan Broder Lund <kristjan.lund@marel.com>
2018-12-06 21:00:00 +0000

    fix: do not cwd in production

--- server.js
+++ server.js
@@ -4,12 +4,6 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable import/first */
 /* eslint-disable new-cap */
-/**
- * Only for apis.is production environment
- */
-if (process.env.NODE_ENV === 'production') {
-  process.chdir('/apis/current')
-}
 
 const { EventEmitter: EE } = require('events')
 const express = require('express')
