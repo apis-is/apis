5f0ec1632ccd9c1f57001c8f87af81e29423bbd7
Valentin Oliver Loftsson <valentinol@advania.is>
2019-02-04 12:06:23 +0000

    Fix lint errors

--- endpoints/aircraft/tests/integration_test.js
+++ endpoints/aircraft/tests/integration_test.js
@@ -22,24 +22,24 @@ describe('aircraft', () => {
   })
   it('should return a 404 when an aircraft is not found', (done) => {
     const params = helpers.testRequestParams('/aircraft', { search: 'loftur' })
-    request.get(params, (error, response, body) => {
+    request.get(params, (error, response) => {
       if (error) {
         return done(error)
       }
-      //const json = JSON.parse(body)
-      //assert.equal(json.error, 'No aircraft found with the query loftur')
+      // const json = JSON.parse(body)
+      // assert.equal(json.error, 'No aircraft found with the query loftur')
       assert.strictEqual(response.statusCode, 404, `Invalid status code. Actual: ${response.statusCode}. Expected: 404`)
       done()
     })
   })
   it('should return a 400 when a search parameter is not provided', (done) => {
     const params = helpers.testRequestParams('/aircraft')
-    request.get(params, (error, response, body) => {
+    request.get(params, (error, response) => {
       if (error) {
         return done(error)
       }
-      //const json = JSON.parse(body)
-      //assert.equal(json.error, 'Please provide a valid search string to lookup')
+      // const json = JSON.parse(body)
+      // assert.equal(json.error, 'Please provide a valid search string to lookup')
       assert.equal(response.statusCode, 400, `Invalid status code. Actual: ${response.statusCode}. Expected: 400`)
       done()
     })
