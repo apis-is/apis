22b153d73c35d85549444dd47a0363a13523e2d9
Pétur Karl IngólfssoN <petur.karl.ingolfsson@gmail.com>
2019-07-26 16:27:15 +0000

    Shooting from the hip

--- endpoints/flight/tests/integration_test.js
+++ endpoints/flight/tests/integration_test.js
@@ -4,7 +4,7 @@ const helpers = require('../../../lib/test_helpers.js')
 
 describe('flight', () => {
   it('should return an array of objects containing correct fields', (done) => {
-    const fieldsToCheckFor = ['date', 'flightNumber', 'airline', 'to', 'plannedArrival', 'realArrival', 'status']
+    const fieldsToCheckFor = ['date', 'flightNumber', 'airline', 'to', 'plannedArrival', 'realArrival', 'status', 'results']
     const params = helpers.testRequestParams('/flight', {
       language: 'en',
       type: 'departures',
