0ad360cabcb4360dc7ed2117d95c31cdd5ae8b9d
Pétur Karl IngólfssoN <petur.karl.ingolfsson@gmail.com>
2019-07-26 16:17:13 +0000

    Fixed some linting errors

--- endpoints/flight/index.js
+++ endpoints/flight/index.js
@@ -3,15 +3,11 @@ const request = require('request')
 const h = require('apis-helpers')
 const _ = require('lodash')
 const moment = require('moment')
-const { parseString } = require('xml2js')
 const app = require('../../server')
 
-
-
 const getFlightData = (parameters, callback) => {
-
   // Default Parameters to upstream endpoint
-  let query = {
+  const query = {
     airport: 'KEF',
     cargo: 0,
     dateFrom: moment().subtract(5, 'minutes').format('YYYY-MM-DD HH:mm'),
@@ -39,14 +35,12 @@ const getFlightData = (parameters, callback) => {
       query.language = 'is'
     }
   }
-
   /* Allowed values are IATA-3 Airport names (only from Iceland) */
   if (parameters.airport) {
     if (parameters.airport.length === 3) {
       query.airport = parameters.airport
     }
   }
-
   /* Allowed values are either 0 or 1 */
   if (parameters.language) {
     if (parameters.language === 'en') {
@@ -56,29 +50,26 @@ const getFlightData = (parameters, callback) => {
       query.language = 'is'
     }
   }
-
-  /* Allowed values datetime format YYYY-MM-DDTHH:mm 
+  /* Allowed values datetime format YYYY-MM-DDTHH:mm
   ** Expected maximum range back is 90 days
   */
   if (parameters.dateFrom) {
-    let dateFrom = moment(parameters.dateFrom, 'YYYY-MM-DD[T]HH:mm')
+    const dateFrom = moment(parameters.dateFrom, 'YYYY-MM-DD[T]HH:mm')
     if (dateFrom.isValid()) {
       query.dateFrom = dateFrom.format('YYYY-MM-DD HH:mm')
     }
   }
-
-  /* Allowed values datetime format YYYY-MM-DDTHH:mm 
+  /* Allowed values datetime format YYYY-MM-DDTHH:mm
   ** Expected maximum range forward is the current flight season (summer/winter) which is usually 30+ days
   */
   if (parameters.dateTo) {
-    let dateTo = moment(parameters.dateTo, 'YYYY-MM-DD[T]HH:mm')
+    const dateTo = moment(parameters.dateTo, 'YYYY-MM-DD[T]HH:mm')
     if (dateTo.isValid()) {
       query.dateTo = dateTo.format('YYYY-MM-DD HH:mm')
     }
   }
-
   const baseUrl = 'https://www.isavia.is/json/flight'
-  let url = encodeURI(
+  const url = encodeURI(
     `${baseUrl}/?airport=${query.airport}&cargo=${query.cargo}&dateFrom=${query.dateFrom}&dateTo=${query.dateTo}&language=${query.language}&departures=${query.departures}`
   )
   let flights = []
@@ -89,7 +80,6 @@ const getFlightData = (parameters, callback) => {
     },
     json: true
   }, (error, response, body) => {
-
     if (error) {
       callback(`Could not fetch data from upstream API endpoint: ${error}`)
     }
@@ -104,29 +94,23 @@ const getFlightData = (parameters, callback) => {
     }
     return callback(false, flights)
   })
-
 }
 
-/**********************************
- *** Version 1 - Legacy ****
-**********************************/
+//  Version 1 - Legacy
 app.get(['/flight', '/flight/v1'], (req, res) => {
   getFlightData(req.query, (error, flights) => {
     if (error) {
-      return res.status(500).json({
-        error: error
-      })
+      return res.status(500).json({ error })
     }
-
-    const legacyFlights = _.map(flights, function(f){
+    const legacyFlights = _.map(flights, f => {
       return {
-        "date": moment(f.Scheduled).format('D. MMM.'),
-        "flightNumber": f.No,
-        "airline": f.Airline,
-        "to": f.OriginDest,
-        "plannedArrival": moment(f.Scheduled).format('HH:mm'),
-        "realArrival": f.Estimated ? moment(f.Estimated).format('HH:mm') : '',
-        "status": f.Status
+        date: moment(f.Scheduled).format('D. MMM.'),
+        flightNumber: f.No,
+        airline: f.Airline,
+        to: f.OriginDest,
+        plannedArrival: moment(f.Scheduled).format('HH:mm'),
+        realArrival: f.Estimated ? moment(f.Estimated).format('HH:mm') : '',
+        status: f.Status
       }
     })
 
@@ -134,17 +118,12 @@ app.get(['/flight', '/flight/v1'], (req, res) => {
     return res.cache(60).json(legacyFlights)
   })
 })
-/**********************************
- *** Version 2 - Moar Data! ****
-**********************************/
 
+//  Version 2 - Moar Data!
 app.get(['/flight/v2'], (req, res) => {
-
   getFlightData(req.query, (error, flights) => {
     if (error) {
-      return res.status(500).json({
-        error: error
-      })
+      return res.status(500).json({ error })
     }
     // Generally we can expect flight data to change every 60 seconds or less
     return res.cache(60).json(flights)
