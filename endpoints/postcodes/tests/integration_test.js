0e75d3db44c09380be5b84c1fa3fdcf9e9a197f9
benediktth <benediktth@postur.is>
2019-10-25 15:11:01 +0000

    Lint

--- endpoints/postcodes/tests/integration_test.js
+++ endpoints/postcodes/tests/integration_test.js
@@ -3,8 +3,8 @@ const helpers = require('../../../lib/test_helpers')
 
 describe('postcodes', () => {
   it('should return an array of objects containing correct fields', (done) => {
-  const fieldsToCheckFor = ['postCode', 'city']
-  const params = helpers.testRequestParams('/postcodes')
+    const fieldsToCheckFor = ['postCode', 'city']
+    const params = helpers.testRequestParams('/postcodes')
     const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
   })
@@ -17,4 +17,4 @@ describe('concerts', () => {
     const resultHandler = helpers.testRequestHandlerForFields(done, fieldsToCheckFor)
     request.get(params, resultHandler)
   })
-  })
+})
